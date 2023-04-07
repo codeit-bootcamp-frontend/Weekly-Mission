df1897c506940a2c916f45790a1a8f614f0ac09a
hayoung-99 <nhy990130@gmail.com>
2023-04-08 02:09:39 +0900

    feat: input focus 이벤트 텍스트 color 설정
    focusin 이벤트가 발생할 때의 스타일링을 따로 css class로 만들어 추가
    이 과정에서 css 선택자 우선순위가 밀리는 이슈가 발생하여 기존의 input 선택자의 우선순위를 낮춤

--- signin/index.js
+++ signin/index.js
@@ -1,13 +1,15 @@
 function focusIn(e) {
   // console.log("focusin 이벤트 발생");
   // console.log(e.target.parentElement);
-  e.target.parentElement.classList.add("focus");
+  e.target.classList.add("focus-text-color");
+  e.target.parentElement.classList.add("focus-outline");
 }
 
 function focusOut(e) {
   // console.log("focusout 이벤트 발생");
   // console.log(e.target.parentElement);
-  e.target.parentElement.classList.remove("focus");
+  e.target.classList.remove("focus-text-color");
+  e.target.parentElement.classList.remove("focus-outline");
 }
 
 let inputNode = document.querySelectorAll(".login-form input");
