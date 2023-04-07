40c1fe8d671324bb48751aec57b61d72dfc88a21
hayoung-99 <nhy990130@gmail.com>
2023-04-08 02:20:47 +0900

    feat: 이메일 input 빈 칸일 때 alert 설정

--- signin/index.js
+++ signin/index.js
@@ -6,6 +6,10 @@ function focusIn(e) {
 function focusOut(e) {
   e.target.classList.remove("focus-text-color");
   e.target.parentElement.classList.remove("focus-outline");
+  
+  if (e.target.id === "email" && e.target.value === "") {
+    alert("이메일을 입력해주세요");
+  }
 }
 
 let inputNode = document.querySelectorAll(".login-form input");
