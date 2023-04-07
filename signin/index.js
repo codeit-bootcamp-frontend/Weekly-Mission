91b95d9dbcfa36061b03f48c8577882d53ef2d56
hayoung-99 <nhy990130@gmail.com>
2023-04-08 02:14:05 +0900

    style: 주석 삭제

--- signin/index.js
+++ signin/index.js
@@ -1,13 +1,9 @@
 function focusIn(e) {
-  // console.log("focusin 이벤트 발생");
-  // console.log(e.target.parentElement);
   e.target.classList.add("focus-text-color");
   e.target.parentElement.classList.add("focus-outline");
 }
 
 function focusOut(e) {
-  // console.log("focusout 이벤트 발생");
-  // console.log(e.target.parentElement);
   e.target.classList.remove("focus-text-color");
   e.target.parentElement.classList.remove("focus-outline");
 }
