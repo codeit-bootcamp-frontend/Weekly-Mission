6fa1fa92944e2a7efa62f3d3ae28f3a9f35f0d9a
redcoin96 <redcoin96@naver.com>
2023-04-07 21:18:40 +0900

    feat(week-3):회원가입 기능구현

---
+++ signup/signup.js
@@ -0,0 +1,71 @@
+const $togglePassword = document.querySelector(".toggle-password");
+const $userEmail = document.querySelector(".userEmail");
+const $userPassword = document.querySelector(".userPassword");
+const $userPasswordVerify = document.querySelector(".userPassword-verify");
+const $signupForm = document.querySelector("#signup-form");
+
+//비밀번호를 확인할 수 있는 아이콘 클릭시 비밀번호의 문자열이 보이기도 하고, 가려지기도 합니다.
+function togglePassword(e) {
+  e.preventDefault();
+  if ($userPassword.type === "password") {
+      $userPassword.type = "text";
+  } else {
+      $userPassword.type = "password";
+  }
+}
+
+/* 1.이메일 input에서 focus out 일 때, 값이 없을 경우 alert으로 “이메일을 입력해주세요.” 메세지를 보입니다.
+   2.이메일 input에서 focus out 일 때, 값이 있고, 이메일 형식에 맞지 않을 경우 alert으로 “올바른 이메일 주소가 아닙니다.” 메세지를 보입니다. 
+   3.이메일 input에서 focus out 일 때, input 값이 test@codeit.com 일 경우, alert으로 “이미 사용 중인 아이디입니다.” 메세지를 보입니다.*/
+function verifyEmail(e) {
+  let target = e.target.value;
+  let regex = new RegExp("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
+
+  if (!target) {
+    alert("이메일을 입력해주세요.");
+  } else if (target === "test@codeit.com") {
+    alert("이미 사용 중인 아이디입니다.");
+  } else if (!regex.test(target)) {
+    alert("올바른 이메일 주소가 아닙니다.");
+  }
+}
+
+//비밀번호 input에서 focus out 일 때, 값이 없거나 문자열만 있거나 숫자만 있는 경우, alert으로 “비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.” 메세지를 보입니다.
+function verifyPassword(e) {
+  let target = e.target.value;
+  let regex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+  if (!regex.test(target)) {
+    alert("비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
+  }
+}
+
+//회원가입을 실행할 경우, 문제가 있는 경우 문제가 있는 부분을 alert 메세지로 알립니다.
+function verifysignupForm(e) {
+  e.preventDefault();
+
+  let email = $userEmail.value;
+  let password = $userPassword.value;
+  let passwordVerify = $userPasswordVerify.value;
+
+  let regexEmail = new RegExp("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
+  let regexPassword = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+
+  if (!email) {
+    alert("이메일을 입력해주세요.");
+  } else if (email === "test@codeit.com") {
+    alert("이미 사용 중인 아이디입니다.");
+  } else if (!regexEmail.test(email)) {
+    alert("올바른 이메일 주소가 아닙니다.");
+  } else if (!regexPassword.test(password)) {
+    alert("비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
+  } else if(password !== passwordVerify){
+    alert("비밀번호 확인이 일치하지 않습니다.");
+  } else {
+    location.href = "./my-link.html";
+  }
+}
+
+$togglePassword.addEventListener("click", togglePassword);
+$userEmail.addEventListener("focusout", verifyEmail);
+$userPassword.addEventListener("focusout", verifyPassword);
+$signupForm.addEventListener("submit", verifysignupForm);
