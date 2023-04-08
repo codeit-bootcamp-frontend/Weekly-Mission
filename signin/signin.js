5b8f1e2a381bbc44b9f6b00c805f77f8e8fd8e30
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-04-08 14:42:41 +0900

    디렉토리 경로 수정 및 모바일 화면, 로그인 페이지 요구사항 구현

---
+++ signin/signin.js
@@ -0,0 +1,30 @@
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+
+function goUrl() {
+  let link = '../my-link.html';
+  location.href = link;
+}
+
+loginForm.onsubmit = function (e) {
+  e.preventDefault();
+  if (emailInput.value === 'test@codeit.com' && passwordInput.value === 'codeit101') {
+    goUrl();
+  } else {
+    alert('이메일과 비밀번호를 확인해주세요');
+  }
+}
+
+function emailVerify() {
+  if (!emailInput.value) {
+    alert('이메일을 입력해주세요!');
+  } else {
+    const reg = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+    if (!reg.test(emailInput.value)) {
+      alert('올바른 이메일 주소가 아닙니다!')
+    }
+  }
+}
+
+emailInput.addEventListener('focusout', emailVerify);
