0c4a75fc793ce95c7521f56fbe0fac98229726bd
hayoung-99 <nhy990130@gmail.com>
2023-04-08 17:52:03 +0900

    feat: signup input별 focus 이벤트 핸들러 분리
    -각 input에 따라 focus 이벤트 핸들러 함수를 정의
    -유효하지 않은 입력인 경우 focusout되지 않고 계속 focus 유지를 하도록 구현:  focusout 핸들러 함수 내부에서 focusout 이벤트 핸들러를 삭제하고, 다시 focusin 됐을 때 focusout 이벤트 핸들러 등록

--- signup/index.js
+++ signup/index.js
@@ -1,44 +1,59 @@
+function addFocusOutEventListener(e) {
+  if (e.target.id === "email") {
+    e.target.addEventListener("focusout", emailFocusOut);
+  } else if (e.target.id === "password") {
+    e.target.addEventListener("focusout", passwordFocusOut);
+  }
+}
+
 function focusIn(e) {
   e.target.classList.add("focus-text-color");
   e.target.parentElement.classList.add("focus-outline");
 
-  if (e.target.id === "password-remind") {
-    const passwordValue = document.querySelector("#password").value;
-
-    if (passwordValue === "") {
-      alert("비밀번호를 먼저 입력해주세요.");
-      e.target.blur();
-    }
-  }
+  addFocusOutEventListener(e);
 }
 
 function focusOut(e) {
   e.target.classList.remove("focus-text-color");
   e.target.parentElement.classList.remove("focus-outline");
+}
 
-  if (e.target.id === "email") {
-    const emailValue = e.target.value;
-    const regexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g;
+function focusEmailInput(e) {
+  e.target.focus();
+  e.target.removeEventListener("focusout", emailFocusOut);
+}
 
-    if (emailValue === "") {
-      alert("이메일을 입력해주세요.");
-    } else if (emailValue === "test@codeit.com") {
-      alert("이미 사용 중인 아이디입니다.");
-    } else if (!regexp.test(emailValue)) {
-      alert("올바른 이메일 주소가 아닙니다.");
-      e.target.value = null;
-    }
-  } else if (e.target.id === "password") {
-    const passwordValue = e.target.value;
-    const emptyPassword = ""; 
-    const checkOnlyNumber = /^[0-9]+$/;
-    const checkOnlyString = /^[\D]+$/;
+function emailFocusOut(e) {
+  const emailValue = e.target.value;
+  const validEmail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g;
 
-    if (passwordValue.length < 8 || passwordValue === emptyPassword || checkOnlyNumber.test(passwordValue) || checkOnlyString.test(passwordValue)) {
-      alert("비밀번호는 영문, 숫자 조합 8자리 이상 입력해 주세요.");
-      e.target.value = null;
-    } 
+  if (emailValue === "") {
+    alert("이메일을 입력해주세요.");
+    focusEmailInput(e);
+  } else if (emailValue === "test@codeit.com") {
+    alert("이미 사용 중인 아이디입니다.");
+    focusEmailInput(e);
+  } else if (!validEmail.test(emailValue)) {
+    alert("올바른 이메일 주소가 아닙니다.");
+    focusEmailInput(e);
   }
+}
+
+function focusPasswordInput(e) {
+  e.target.focus();
+  e.target.removeEventListener("focusout", passwordFocusOut);
+}
+
+function passwordFocusOut(e) {
+  const passwordValue = e.target.value;
+  const emptyPassword = ""; 
+  const checkOnlyNumber = /^[0-9]+$/;
+  const checkOnlyString = /^[\D]+$/;
+
+  if (passwordValue.length < 8 || passwordValue === emptyPassword || checkOnlyNumber.test(passwordValue) || checkOnlyString.test(passwordValue)) {
+    alert("비밀번호는 영문, 숫자 조합 8자리 이상 입력해 주세요.");
+    focusPasswordInput(e);
+  } 
 }
 
 const inputNodes = document.querySelectorAll("input");
@@ -46,3 +61,10 @@ inputNodes.forEach((inputNode) => {
   inputNode.addEventListener("focusin", focusIn);
   inputNode.addEventListener("focusout", focusOut);
 })
+
+const emailInput = inputNodes[0];
+const passwordInput = inputNodes[1];
+const passwordRemindInput = inputNodes[2];
+
+emailInput.addEventListener("focusout", emailFocusOut);
+passwordInput.addEventListener("focusout", passwordFocusOut);
