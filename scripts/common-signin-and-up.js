6bec98f9cce49270dffda1b83b20c9fec9caac64
drizzle96 <dlxkffldk37@naver.com>
2023-04-08 14:00:11 +0900

    feat(email): email 형식 체크 기능 구현

--- scripts/common-signin-and-up.js
+++ scripts/common-signin-and-up.js
@@ -1,4 +1,6 @@
 const form = document.querySelector("form");
+const emailFormat = /[\w\-\.]+\@[\w\-]+\.\w/g;
+
 
 function addFocusStyle(e) {
   if (e.target.classList.contains("input")) {
@@ -12,5 +14,19 @@ function removeFocusStyle(e) {
   }
 }
 
+function checkEmailFormat(e) {
+  if (e.target.id === "email") {
+    const emailValue = e.target.value
+    if (emailValue == "") {
+      alert("이메일을 입력해주세요.")
+    }
+    else if (emailValue.search(emailFormat) === -1) {
+      alert("올바른 이메일 주소가 아닙니다.")
+    }
+  }
+}
+
 form.addEventListener('focusin', addFocusStyle);
+
 form.addEventListener('focusout', removeFocusStyle);
+form.addEventListener('focusout', checkEmailFormat);
