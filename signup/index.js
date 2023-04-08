5443d3ffe7091be4db3658025b26977eb5e6aecd
hayoung-99 <nhy990130@gmail.com>
2023-04-08 16:20:23 +0900

    feat: 비밀번호 정규표현식으로 유효성 검토
    이메일과 마찬가지로 유효하지 않은 비밀번호일 경우에 기존에 입력한 값을 초기화

--- signup/index.js
+++ signup/index.js
@@ -8,16 +8,27 @@ function focusOut(e) {
   e.target.parentElement.classList.remove("focus-outline");
 
   if (e.target.id === "email") {
+    const emailValue = e.target.value;
     const regexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g;
 
-    if (e.target.value === "") {
+    if (emailValue === "") {
       alert("이메일을 입력해주세요.");
-    } else if (e.target.value === "test@codeit.com") {
+    } else if (emailValue === "test@codeit.com") {
       alert("이미 사용 중인 아이디입니다.");
-    } else if (!regexp.test(e.target.value)) {
+    } else if (!regexp.test(emailValue)) {
       alert("올바른 이메일 주소가 아닙니다.");
       e.target.value = null;
     }
+  } else if (e.target.id === "password") {
+    const passwordValue = e.target.value;
+    const emptyPassword = ""; 
+    const checkOnlyNumber = /^[0-9]+$/;
+    const checkOnlyString = /^[\D]+$/;
+
+    if (passwordValue.length < 8 || passwordValue === emptyPassword || checkOnlyNumber.test(passwordValue) || checkOnlyString.test(passwordValue)) {
+      alert("비밀번호는 영문, 숫자 조합 8자리 이상 입력해 주세요.");
+      e.target.value = null;
+    } 
   }
 }
 
