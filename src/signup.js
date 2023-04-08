fc7da267f5b0aa6c76f24f7f5c7a5443a62ff051
SeyoungCho <wnl383@naver.com>
2023-04-08 23:13:06 +0900

    feat: 비밀번호 인풋에 이벤트리스너 추가
    비밀번호 인풋 focusout시
    - 비밀번호 유효성검사

--- src/signup.js
+++ src/signup.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const usernameInput = document.getElementById("username");
+  const passwordInput = document.querySelector("#password");
 
   const validateEmail = (mail) => {
     const mailformat =
       /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (mail.match(mailformat)) {
+      return true;
+    }
+    return false;
+  };
+
+  const validatePassword = (password) => {
+    const passwordFormat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (password.match(passwordFormat)) {
       return true;
     }
     return false;
@@ -21,6 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     if (e.target.value === "test@codeit.com") {
       alert("이미 사용 중인 아이디입니다.");
+      return;
+    }
+  });
+
+  passwordInput.addEventListener("focusout", (e) => {
+    if (!validatePassword(e.target.value)) {
+      alert("비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
       return;
     }
   });
