40e5ef64cb117779eb95bebd23312db64442a107
hayoung-99 <nhy990130@gmail.com>
2023-04-08 15:12:04 +0900

    feat: 이메일 정규표현식으로 유효성 검토
    이메일 형식이 올바르지 않은 경우 기존에 입력한 이메일 값을 초기화

--- signin/index.js
+++ signin/index.js
@@ -1,3 +1,5 @@
+let flag = false;
+
 function focusIn(e) {
   e.target.classList.add("focus-text-color");
   e.target.parentElement.classList.add("focus-outline");
@@ -7,8 +9,15 @@ function focusOut(e) {
   e.target.classList.remove("focus-text-color");
   e.target.parentElement.classList.remove("focus-outline");
   
-  if (e.target.id === "email" && e.target.value === "") {
-    alert("이메일을 입력해주세요.");
+  if (e.target.id === "email") {
+    const regexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g;
+
+    if (e.target.value === "") {
+      alert("이메일을 입력해주세요.");
+    } else if (!regexp.test(e.target.value)) {
+      alert("올바른 이메일 주소가 아닙니다.");
+      e.target.value = null;
+    }
   }
 }
 
