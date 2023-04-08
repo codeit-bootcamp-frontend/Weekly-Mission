29383d456bf2619d9716a3a414017cb1aff3d2da
hayoung-99 <nhy990130@gmail.com>
2023-04-08 14:29:36 +0900

    feat: 이메일 정규표현식 생성 & 이메일 유효성 확인

--- signup/index.js
+++ signup/index.js
@@ -8,11 +8,14 @@ function focusOut(e) {
   e.target.parentElement.classList.remove("focus-outline");
 
   if (e.target.id === "email") {
+    let regex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g;
+
     if (e.target.value === "") {
       alert("이메일을 입력해주세요.");
-    }
-    else if (e.target.value === "test@codeit.com") {
+    } else if (e.target.value === "test@codeit.com") {
       alert("이미 사용 중인 아이디입니다.");
+    } else if (!regex.test(e.target.value)) {
+      alert("올바른 이메일 주소가 아닙니다.");
     }
   }
 }
