d28cd0ef16588af9e236a8954c9893b78ed855b5
SeyoungCho <wnl383@naver.com>
2023-04-08 21:00:23 +0900

    feat: 이메일 input에 이벤트리스너 추가
    focusout 시 먼저 공란 체크 후 형식 체크

--- src/signin.js
+++ src/signin.js
@@ -1,1 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const usernameInput = document.getElementById("username");
+  const showPasswordBtn = document.getElementById("show-password-icon");
+  const loginBtn = document.getElementById("login-btn");
+
+  const validateEmail = (mail) => {
+    const mailformat =
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (mail.match(mailformat)) {
+      return true;
+    }
+    return false;
+  };
+
+  usernameInput.addEventListener("focusout", (e) => {
+    if (!e.target.value) {
+      alert("이메일을 입력해주세요.");
+      return;
+    }
+    if (!validateEmail(e.target.value)) {
+      alert("올바른 이메일 주소가 아닙니다.");
+      return;
+    }
+  });
+});
