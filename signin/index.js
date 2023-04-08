f8065de4ba2bb4508d97f85d7b93fa50c003418b
hayoung-99 <nhy990130@gmail.com>
2023-04-08 18:48:45 +0900

    feat: signin 이메일 input focus 이벤트 핸들러 분리
    이메일 입력값이 유효하지 않은 경우, 계속 focusin 유지되도록 구현
    이슈 발생: 유효한 값을 입력하지 않으면 계속 focus 유지되어 다른 요소 focus 불가능
    
    관련 commit: 0c4a

--- signin/index.js
+++ signin/index.js
@@ -3,6 +3,10 @@ let flag = false;
 function focusIn(e) {
   e.target.classList.add("focus-text-color");
   e.target.parentElement.classList.add("focus-outline");
+
+  if (e.target.id === "email") {
+    e.target.addEventListener("focusout", focusOut);
+  }
 }
 
 function focusOut(e) {
@@ -14,11 +18,17 @@ function focusOut(e) {
 
     if (e.target.value === "") {
       alert("이메일을 입력해주세요.");
+      focusEmailInput(e);
     } else if (!regexp.test(e.target.value)) {
       alert("올바른 이메일 주소가 아닙니다.");
-      e.target.value = null;
+      focusEmailInput(e);
     }
   }
+}
+
+function focusEmailInput(e) {
+  e.target.focus();
+  e.target.removeEventListener("focusout", focusOut);
 }
 
 let inputNode = document.querySelectorAll(".login-form input");
