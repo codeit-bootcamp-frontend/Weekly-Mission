8b8d3b9d567f50281c5481478b93283597a97b7a
임병욱 <bwlimtony@gmail.com>
2023-04-09 23:02:20 +0900

    refactor: change 이벤트 리스너 삭제, 비밀번호 유효성을 위한 flag 변수 설정

--- pages/signin/signin.js
+++ pages/signin/signin.js
@@ -1,23 +1,32 @@
 const userEmail = document.getElementById('user-email');
 const userPassword = document.getElementById('user-password');
-const submitForm = document.querySelector('form');
 const inputs = document.querySelectorAll('input');
 const form = document.querySelector('form');
 const togglePassword = document.querySelector('#togglePassword');
 
-let enteredEmail = userEmail.value;
-let enteredPassword = userPassword.value;
+let enteredEmail;
+let enteredPassword;
+
+let passValidationEmail = false;
+
+// 이메일 규칙
+const emailRegexp =
+  /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+
+// 비밀번호 규칙
+const passwordRegexp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 // 유효성 검사
-function validation(e) {
-  e.preventDefault();
-  if (enteredEmail === 'text@codeit.com' && enteredPassword === 'codeit101') {
-    location.href = '/my-link';
+function validation(event) {
+  event.preventDefault();
+  if (enteredEmail === 'test@codeit.com' && enteredPassword === 'codeit101') {
+    window.location.href = '/signup';
   } else {
     alert('이메일과 비밀번호를 확인해주세요.');
   }
 }
 
+//input focus in and focue out register eventlistner
 form.addEventListener('focusin', (event) => {
   const targetEl = event.target;
   targetEl.style.color = 'black';
@@ -31,19 +40,28 @@ form.addEventListener('focusout', (event) => {
 });
 
 // 이메일 유효성 검사
-function enterEmail() {
-  if (!enteredEmail.includes('@')) alert('올바른 이메일 주소가 아닙니다.');
-  else if (!enteredEmail.length) alert('이메일을 입력해주세요');
+function enterEmail(event) {
+  enteredEmail = userEmail.value;
+  if (!enteredEmail.length) {
+    alert('이메일을 입력해주세요');
+    return;
+  } else if (!emailRegexp.test(enteredEmail)) {
+    alert('올바른 이메일 주소가 아닙니다.');
+    return;
+  }
+  passValidationEmail = true;
 }
 
-// input 최신값 반영
-userEmail.addEventListener('change', (event) => {
-  enteredEmail = event.target.value;
-});
-
-userPassword.addEventListener('change', (event) => {
-  enteredEmail = event.target.value;
-});
+// 비밀번호 유효성 검시
+function enterPassword(event) {
+  enteredPassword = userPassword.value();
+  if (
+    passValidationEmail &&
+    (!passwordRegexp.test(enteredPassword) || !enteredPassword.length)
+  ) {
+    alert('비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
+  }
+}
 
 // input eye 토글
 togglePassword.addEventListener('click', function () {
@@ -57,4 +75,5 @@ togglePassword.addEventListener('click', function () {
 });
 
 userEmail.addEventListener('focusout', enterEmail);
-submitForm.addEventListener('submit', validation);
+userPassword.addEventListener('focusout', enterPassword);
+form.addEventListener('submit', validation);
--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -1,80 +1,96 @@
 const userEmail = document.getElementById('user-email');
 const userPassword = document.getElementById('user-password');
 const checkUserPassword = document.getElementById('user-check-password');
-const togglePasswords = document.querySelectorAll('#togglePassword');
 const submitForm = document.querySelector('form');
 const passwordBox = document.querySelector('.container .password-box');
 const form = document.querySelector('form');
 
-let enteredEmail = userEmail.value;
-let enteredPassword = userPassword.value;
-let enteredChcekPassword = checkUserPassword.value;
+let enteredEmail;
+let enteredPassword;
+let enteredChcekPassword;
+
+let passValidationEmail = false;
 
 // 이메일 규칙
-const checkPasswordRegexp = /^[a-zA-Z0-9]/g;
+const emailRegexp =
+  /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
-// 로그인 완료
+// 비밀번호 규칙
+const passwordRegexp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+// 유효성 검사
 function validation(e) {
   e.preventDefault();
-  if (enteredPassword === enteredChcekPassword) location.href = '/my-link';
+  if (enteredEmail === 'test@codeit.com') {
+    alert('이미 사용 중인 아이디입니다.');
+  } else if (enteredPassword === enteredChcekPassword)
+    location.href = '/my-link';
   else alert('비밀번호가 일치하지 않습니다.');
 }
 
-// 이메일 유효성 검사
-function validateEmail() {
-  if (enteredEmail === 'text@codeit.com') {
-    alert('이미 사용 중인 아이디입니다.');
-  } else if (!enteredEmail.includes('@'))
-    alert('올바른 이메일 주소가 아닙니다.');
-  else if (!enteredEmail.length) alert('이메일을 입력해주세요');
-}
-
-// 비밀번호 유효성 검사
-function validatePassword() {
-  if (!enteredPassword.length || checkPasswordRegexp.test(enteredPassword))
-    alert('비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
-}
-
-// 최신값 반영
-userEmail.addEventListener('change', (event) => {
-  enteredEmail = event.target.value;
-});
-
-userPassword.addEventListener('change', (event) => {
-  enteredPassword = event.target.value;
-});
-
-checkUserPassword.addEventListener('change', (event) => {
-  enteredChcekPassword = event.target.value;
-});
-
-// inputs focus in
+//input focus in and focue out register eventlistner
 form.addEventListener('focusin', (event) => {
   const targetEl = event.target;
   targetEl.style.color = 'black';
   targetEl.style.border = '1px solid #6D6AFE';
 });
-// inputs focus out
+
 form.addEventListener('focusout', (event) => {
   const targetEl = event.target;
   targetEl.style.color = 'gray';
   targetEl.style.border = '1px solid gray';
 });
 
-// eye icon toggle
-passwordBox.addEventListener('click', (event) => {
+// 이메일 유효성 검사
+function validateEmail() {
+  enteredEmail = userEmail.value;
+  if (enteredEmail === 'test@codeit.com') {
+    alert('이미 사용 중인 아이디입니다.');
+    return;
+  } else if (!enteredEmail.length) {
+    alert('이메일을 입력해주세요');
+    return;
+  } else if (!emailRegexp.test(enteredEmail)) {
+    alert('올바른 이메일 주소가 아닙니다.');
+    return;
+  }
+  passValidationEmail = true;
+}
+
+// 비밀번호 유효성 검사
+function validatePassword() {
+  enteredPassword = userPassword.value;
+  if (
+    passValidationEmail &&
+    (!enteredPassword.length || !passwordRegexp.test(enteredPassword))
+  )
+    alert('비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
+}
+
+function validateCheckPassword() {
+  enteredChcekPassword = checkUserPassword.value;
+  if (
+    passValidationEmail &&
+    (!enteredChcekPassword.length || !passwordRegexp.test(enteredChcekPassword))
+  )
+    alert('비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
+}
+
+function toggleEye(event) {
   const targetEl = event.target;
   const type =
     targetEl.previousElementSibling.getAttribute('type') === 'password'
       ? 'text'
       : 'password';
   targetEl.previousElementSibling.setAttribute('type', type);
-
   targetEl.classList.toggle('bi-eye');
-});
+}
+
+// eye icon toggle
+passwordBox.addEventListener('click', toggleEye);
 
 userEmail.addEventListener('focusout', validateEmail);
 userPassword.addEventListener('focusout', validatePassword);
-checkUserPassword.addEventListener('focusout', validatePassword);
+checkUserPassword.addEventListener('focusout', validateCheckPassword);
 
 submitForm.addEventListener('submit', validation);
