dd607aaa53eb1a2d6f3a881a7fc6b8cbf2b8e0d0
redcoin96 <redcoin96@naver.com>
2023-04-09 18:42:36 +0900

    feat(week-3):공백추가

--- signin/signin.js
+++ signin/signin.js
@@ -6,7 +6,6 @@ const $loginForm = document.querySelector('#login-form')
 
 //비밀번호를 확인할 수 있는 아이콘 클릭시 비밀번호의 문자열이 보이기도 하고, 가려지기도 합니다.
 function togglePassword(e) {
-
     e.preventDefault();
 
     const icons = e.target.parentNode.children;
@@ -26,7 +25,6 @@ function togglePassword(e) {
 /* 1.이메일 input에서 focus out 일 때, 값이 없을 경우 alert으로 “이메일을 입력해주세요.” 메세지를 보입니다.
    2.이메일 input에서 focus out 일 때, 값이 있고, 이메일 형식에 맞지 않을 경우 alert으로 “올바른 이메일 주소가 아닙니다.” 메세지를 보입니다. */
 function verifyEmail(e){
-
     let target = e.target.value;
     let regex = new RegExp("^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$");
 
@@ -34,10 +32,9 @@ function verifyEmail(e){
         alert('이메일을 입력해주세요.')
     }
     else if(!regex.test(target)){
-        alert('올바른 이메일 주소가 아닙니다.')
+        alert('올바른 이메일 주소가 아닙니다.');
     }
 }
-
 
 function verifyLogininfo(e){
     e.preventDefault();
