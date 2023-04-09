d74adb4c215a1597b6561dc4d13a562fe6f74c6f
Yejin Shin <shinyejin0212@gmail.com>
2023-04-09 23:19:45 +0900

    refactor: 불필요한 removeEventListner 제거

--- signup/signup.js
+++ signup/signup.js
@@ -14,15 +14,13 @@ function isValidEmail(e) {
 
   if (email.value === "") {
     alert("이메일을 입력해주세요.");
-    removeFocusOutEventListener(e);
+    // removeFocusOutEventListener(e);
     return false;
   } else if (!email.value.match(emailRegex)) {
     alert("올바른 이메일 주소가 아닙니다.");
-    removeFocusOutEventListener(e);
     return false;
   } else if (email.value === "test@codeit.com") {
     alert("이미 사용 중인 아이디입니다.");
-    removeFocusOutEventListener(e);
     return false;
   }
   return true;
@@ -37,7 +35,6 @@ function isValidPassword(e) {
 
   if (!password.value.match(passwordRegex)) {
     alert("비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
-    removeFocusOutEventListener(e);
     return false;
   }
   return true;
@@ -57,16 +54,6 @@ function addFocusOutEventListener(e) {
     e.target.addEventListener("focusout", isValidEmail);
   } else if (e.target === password) {
     e.target.addEventListener("focusout", isValidPassword);
-  }
-}
-
-function removeFocusOutEventListener(e) {
-  if (e.target === email) {
-    e.target.removeEventListener("focusout", isValidEmail);
-    e.preventDefault();
-  } else if (e.target === password) {
-    e.target.removeEventListener("focusout", isValidPassword);
-    e.preventDefault();
   }
 }
 
