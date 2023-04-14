e9f1edb00dcf69fcfa98cfc593cefc9207bcb1d8
redcoin96 <redcoin96@naver.com>
2023-04-14 12:51:46 +0900

    feat(mentor): 들여쓰기 2칸으로 변경

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,7 @@
 const $togglePasswordIcon = document.querySelector(".toggle-password");
-const $togglePasswordVerifyIcon = document.querySelector(".toggle-passwordverify");
+const $togglePasswordVerifyIcon = document.querySelector(
+  ".toggle-passwordverify"
+);
 const $userEmailInput = document.querySelector(".user-email");
 const $userPasswordInput = document.querySelector(".user-password");
 const $userPasswordVerifyInput = document.querySelector(".user-passwordverify");
@@ -9,14 +11,14 @@ const $signupForm = document.querySelector("#signup-form");
 function togglePassword(e) {
   e.preventDefault();
   const icons = e.target.parentNode.children;
-  const input = e.target.closest('div').querySelector('input');
-  for(let i = 0; i<icons.length; i++){
-      icons[i].classList.toggle('hide')
+  const input = e.target.closest("div").querySelector("input");
+  for (let i = 0; i < icons.length; i++) {
+    icons[i].classList.toggle("hide");
   }
   if (input.type === "password") {
-      input.type = "text";
+    input.type = "text";
   } else {
-      input.type = "password";
+    input.type = "password";
   }
 }
 
@@ -24,7 +26,7 @@ function togglePassword(e) {
    2.이메일 input에서 focus out 일 때, 값이 있고, 이메일 형식에 맞지 않을 경우 alert으로 “올바른 이메일 주소가 아닙니다.” 메세지를 보입니다. 
    3.이메일 input에서 focus out 일 때, input 값이 test@codeit.com 일 경우, alert으로 “이미 사용 중인 아이디입니다.” 메세지를 보입니다.*/
 function verifyEmail(e) {
-  if(e.sourceCapabilities){
+  if (e.sourceCapabilities) {
     let target = e.target.value;
     let regex = new RegExp("^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$");
     if (!target) {
@@ -39,7 +41,7 @@ function verifyEmail(e) {
 
 //비밀번호 input에서 focus out 일 때, 값이 없거나 문자열만 있거나 숫자만 있는 경우, alert으로 “비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.” 메세지를 보입니다.
 function verifyPassword(e) {
-  if(e.sourceCapabilities){
+  if (e.sourceCapabilities) {
     let target = e.target.value;
     let regex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
     if (!regex.test(target) && !$togglePasswordIcon.contains(e.relatedTarget)) {
@@ -54,7 +56,9 @@ function verifysignupForm(e) {
   let email = $userEmailInput.value;
   let password = $userPasswordInput.value;
   let passwordVerify = $userPasswordVerifyInput.value;
-  let regexEmail = new RegExp("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
+  let regexEmail = new RegExp(
+    "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"
+  );
   let regexPassword = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
   if (!email) {
     alert("이메일을 입력해주세요.");
@@ -64,7 +68,7 @@ function verifysignupForm(e) {
     alert("올바른 이메일 주소가 아닙니다.");
   } else if (!regexPassword.test(password)) {
     alert("비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
-  } else if(password !== passwordVerify){
+  } else if (password !== passwordVerify) {
     alert("비밀번호 확인이 일치하지 않습니다.");
   } else {
     location.href = "../my-link/my-link.html";
