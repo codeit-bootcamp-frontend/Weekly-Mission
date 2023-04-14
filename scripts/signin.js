db1cbe558400e4b177cba21daede34e7ca5b7286
drizzle96 <dlxkffldk37@naver.com>
2023-04-14 14:45:39 +0900

    chore(jonghoonoh: 코드리뷰 반영 및 .js 파일 prettier 실행
    1. 로그인 페이지 이동 href → replace 변경
    2. 포맷 정규식 확인 함수 search → test 변경
    3. js 파일 prettier 실행

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,54 +1,52 @@
-const signinForm = document.querySelector("form")
-const signinEmail = document.querySelector("#email")
-const signinPassword = document.querySelector("#password")
-const icon = document.querySelector(".view-password")
+const signinForm = document.querySelector("form");
+const signinEmail = document.querySelector("#email");
+const signinPassword = document.querySelector("#password");
+const icon = document.querySelector(".view-password");
 
-const emailFormat = /^[\w\-\.\/]+\@[\w\-]+\.[\w]+$/
-const passwordFormat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+const emailFormat = /^[\w\-\.\/]+\@[\w\-]+\.[\w]+$/;
+const passwordFormat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 function checkEmailFormat(email) {
   if (!email) {
-    alert("이메일을 입력해주세요.")
-    return false
-  } else if (email.search(emailFormat) === -1) {
-    alert("올바른 이메일 주소가 아닙니다.")
-    return false
+    alert("이메일을 입력해주세요.");
+    return false;
+  } else if (!emailFormat.test(email)) {
+    alert("올바른 이메일 주소가 아닙니다.");
+    return false;
   }
-  return true
+  return true;
 }
 
 function checkMemberAccount(email, password) {
-  return (email === "test@codeit.com") && (password === "codeit101")
+  return email === "test@codeit.com" && password === "codeit101";
 }
 
 function checkEmail(e) {
-  const email = signinEmail.value
-  checkEmailFormat(email)
+  const email = signinEmail.value;
+  checkEmailFormat(email);
 }
 
 function checkSignin(e) {
-  e.preventDefault()
-  const email = signinEmail.value
-  const password = signinPassword.value
+  e.preventDefault();
+  const email = signinEmail.value;
+  const password = signinPassword.value;
 
   if (checkMemberAccount(email, password)) {
-    location.href = "/my-link.html"
-  }
-  else {
-    alert("이메일과 비밀번호를 확인해주세요.")
+    location.replace("/my-link.html");
+  } else {
+    alert("이메일과 비밀번호를 확인해주세요.");
   }
 }
 
 function changePasswordType(e) {
-  e.preventDefault()
+  e.preventDefault();
   if (signinPassword.type === "password") {
-    signinPassword.type = "text"
-  }
-  else {
-    signinPassword.type = "password"
+    signinPassword.type = "text";
+  } else {
+    signinPassword.type = "password";
   }
 }
 
-signinEmail.addEventListener('focusout', checkEmail)
-signinForm.addEventListener('submit', checkSignin)
-icon.addEventListener('mousedown', changePasswordType)
+signinEmail.addEventListener("focusout", checkEmail);
+signinForm.addEventListener("submit", checkSignin);
+icon.addEventListener("mousedown", changePasswordType);
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,99 +1,101 @@
-const signupForm = document.querySelector("form")
-const signupEmail = document.querySelector("#email")
-const signupPassword = document.querySelector("#password")
-const signupPasswordRepeat = document.querySelector("#check-password")
-const icons = document.querySelectorAll(".view-password")
+const signupForm = document.querySelector("form");
+const signupEmail = document.querySelector("#email");
+const signupPassword = document.querySelector("#password");
+const signupPasswordRepeat = document.querySelector("#check-password");
+const icons = document.querySelectorAll(".view-password");
 
-const emailFormat = /^[\w\-\.\/]+\@[\w\-]+\.[\w]+$/
-const passwordFormat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+const emailFormat = /^[\w\-\.\/]+\@[\w\-]+\.[\w]+$/;
+const passwordFormat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 function checkEmailFormat(email) {
   if (!email) {
-    alert("이메일을 입력해주세요.")
-    return false
-  } else if (email.search(emailFormat) === -1) {
-    alert("올바른 이메일 주소가 아닙니다.")
-    return false
+    alert("이메일을 입력해주세요.");
+    return false;
+  } else if (!emailFormat.test(email)) {
+    alert("올바른 이메일 주소가 아닙니다.");
+    return false;
   }
-  return true
+  return true;
 }
 
 function checkEmailExist(email) {
   if (email === "test@codeit.com") {
-    alert("이미 사용 중인 아이디입니다.")
-    return false
+    alert("이미 사용 중인 아이디입니다.");
+    return false;
   }
-  return true
+  return true;
 }
 
 function checkPasswordFormat(password) {
-  if (password.search(passwordFormat) === -1) {
-    alert("비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.")
-    return false
+  if (!passwordFormat.test(password)) {
+    alert("비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
+    return false;
   }
-  return true
+  return true;
 }
 
 function checkPasswordRepeatSame(password, passwordRepeat) {
   if (password !== passwordRepeat) {
-    alert("비밀번호가 동일하지 않습니다.")
-    return false
+    alert("비밀번호가 동일하지 않습니다.");
+    return false;
   }
-  return true
+  return true;
 }
 
 function checkEmail(e) {
   if (e.sourceCapabilities) {
-    const email = signupEmail.value
-    checkEmailFormat(email)
-    checkEmailExist(email)
+    const email = signupEmail.value;
+    checkEmailFormat(email);
+    checkEmailExist(email);
   }
-  
 }
 
 function checkPassword(e) {
   if (e.sourceCapabilities) {
-    const password = signupPassword.value
-    checkPasswordFormat(password)
+    const password = signupPassword.value;
+    checkPasswordFormat(password);
   }
 }
 
 function checkPasswordRepeat(e) {
   if (e.sourceCapabilities) {
-    const password = signupPassword.value
-    const passwordRepeat = signupPasswordRepeat.value
-    checkPasswordRepeatSame(password, passwordRepeat)
+    const password = signupPassword.value;
+    const passwordRepeat = signupPasswordRepeat.value;
+    checkPasswordRepeatSame(password, passwordRepeat);
   }
 }
 
 function checkSignup(e) {
-  e.preventDefault()
-  const email = signupEmail.value
-  const password = signupPassword.value
-  const passwordRepeat = signupPasswordRepeat.value
+  e.preventDefault();
+  const email = signupEmail.value;
+  const password = signupPassword.value;
+  const passwordRepeat = signupPasswordRepeat.value;
 
-  if (checkEmailFormat(email) && checkEmailExist(email) 
-  && checkPasswordFormat(password) && checkPasswordRepeatSame(password, passwordRepeat)) {
-    location.href = "/my-link.html"
+  if (
+    checkEmailFormat(email) &&
+    checkEmailExist(email) &&
+    checkPasswordFormat(password) &&
+    checkPasswordRepeatSame(password, passwordRepeat)
+  ) {
+    location.href = "/my-link.html";
   }
 }
 
 function changePasswordType(e) {
-  e.preventDefault()
-  const passwordInput = e.target.previousElementSibling
+  e.preventDefault();
+  const passwordInput = e.target.previousElementSibling;
   if (passwordInput.type === "password") {
-    passwordInput.type = "text"
-  }
-  else {
-    passwordInput.type = "password"
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
   }
 }
 
-signupEmail.addEventListener('focusout', checkEmail)
-signupPassword.addEventListener('focusout', checkPassword)
-signupPasswordRepeat.addEventListener('focusout', checkPasswordRepeat)
-signupForm.addEventListener('submit', checkSignup)
+signupEmail.addEventListener("focusout", checkEmail);
+signupPassword.addEventListener("focusout", checkPassword);
+signupPasswordRepeat.addEventListener("focusout", checkPasswordRepeat);
+signupForm.addEventListener("submit", checkSignup);
 
-for(let icon of icons) {
-  icon.addEventListener('mousedown', changePasswordType)
+for (let icon of icons) {
+  icon.addEventListener("mousedown", changePasswordType);
 }
