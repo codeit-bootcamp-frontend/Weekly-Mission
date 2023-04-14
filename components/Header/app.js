17d3e04a0e13c6de91643d4051f1ac7365c6efe9
wooleejaan <wooleejaan@gmail.com>
2023-04-14 23:28:08 +0900

    feat: "/" 경로에 header 컴포넌트 삽입

--- components/Header/app.js
+++ components/Header/app.js
@@ -1,6 +1,10 @@
 export default class Header extends HTMLElement {
   constructor() {
     super();
+
+    this.isLoggedIn = false;
+    this.user = JSON.parse(sessionStorage.getItem("currentUser"));
+    this.user ? (this.isLoggedIn = true) : (this.isLoggedIn = false);
   }
   connectedCallback() {
     this.attachShadow({ mode: "open" });
@@ -9,10 +13,14 @@ export default class Header extends HTMLElement {
           <a href="/" class="logo">
             <img src="/static/public/linkbrary-logo.svg" alt="Linkbrary Logo" />
           </a>
-          <a class="user">
-            <img src="/static/public/codeit-profile.svg" alt="User Icon" />
-            <span>Codeit@codeit.com</span>
-          </a>
+          ${
+            !this.isLoggedIn
+              ? `<a href="/pages/signin" class="signin">로그인</a>`
+              : `<a class="user">
+                  <img src="/static/public/codeit-profile.svg" alt="User Icon" />
+                  <span>${this.user.email}</span>
+                </a>`
+          }
         </nav>
       <style>
         nav {
@@ -47,12 +55,47 @@ export default class Header extends HTMLElement {
           align-items: center;
           color: #3E3E43;
         }
+        nav .signin {
+          box-sizing: border-box;
+          text-decoration: none;
+          width: 128px;;
+          height: 53px;
+          background: linear-gradient(
+            90.99deg,
+            var(--linkbrary-primary) 0.12%,
+            #6ae3fe 101.84%
+          );
+          border-radius: 8px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 16px 20px;
+          color: #f5f5f5;
+          font-weight: 600;
+          font-size: 18px;
+          line-height: 21px;
+        }
+        @media (max-width: 863px) {
+          nav .logo {
+            width: 100px;
+          }
+          nav .signin {
+            width: 100px;
+          }
+        }
         @media (max-width: 767px) {
           nav .logo {
-            width: 88.67px;
+            width: 77.58px;
           }
           nav .user span {
             display: none;
+          }
+          nav .signin {
+            width: 80px;
+            height: 37px;
+            font-size: 14px;
+            line-height: 16.71px;
+            padding: 10px 16px;
           }
         }
       </style>
