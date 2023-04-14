c3563e1f13daa13b202f9029aea4ebb0bde341ef
wooleejaan <wooleejaan@gmail.com>
2023-04-14 14:15:00 +0900

    style: 폴더 구조 변경 (/pages/shared/)

---
+++ pages/shared/app.js
@@ -0,0 +1,9 @@
+import Card from "../../components/Card/app.js";
+import Header from "../../components/Header/app.js";
+
+(function () {
+  const init = () => {
+    // document.body.append(new Card(1));
+  };
+  init();
+})();
