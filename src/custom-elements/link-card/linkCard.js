7d9ce8d61e77f66269ba757825121825efbfab65
Seyoung Cho <seycho@cx2r8s4.42seoul.kr>
2023-04-15 16:32:37 +0900

    feat: 카드 컴포넌트 제작

---
+++ src/custom-elements/link-card/linkCard.js
@@ -0,0 +1,217 @@
+export class LinkCard extends HTMLElement {
+  // 컴포넌트 정보를 담고 있는 프로퍼티
+  #prop = {
+    href: "https://www.codeit.kr",
+    thumbnailSrc: "/images/default-thumbnail.svg",
+    isLiked: false,
+    metadata: {
+      updatedTime: new Date(),
+      description:
+        "Lorem ipsum dolor sit amet consectetur. Metus amet habitant nunc consequat. Lorem ipsum dolor sit amet consectetur. Metus amet habitant nunc consequat.",
+      date: new Date(2023, 3, 13).toLocaleDateString().slice(0, -1),
+    },
+  };
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  /**
+   * 컴포넌트가 DOM트리에 노드로 추가되었을 때 호출되는 콜백
+   */
+  connectedCallback() {
+    this.render();
+  }
+
+  disconnectedCallback() {
+    this.likeBtn.removeEventListener();
+  }
+
+  get prop() {
+    return this.#prop;
+  }
+
+  set prop(value) {
+    this.#prop = value;
+
+    this.thumbnailImg.setAttribute("src", value.thumbnailSrc);
+    this.descriptionDiv.textContent = value.metadata.description;
+    this.dateP.textContent = value.metadata.date;
+  }
+
+  get styles() {
+    return `
+      * {
+        box-sizing: border-box;
+		text-decoration: none;
+      }
+
+	  div, p {
+		margin: 0;
+	  }
+
+      :host {
+        display: inline-block;
+      }
+
+      .card-container {
+		position: relative;
+        width: 21.25rem;
+        height: 20.875rem;
+        box-shadow: 0px 5px 25px rgba(0, 0, 0, 0.08);
+		border-radius: 0.9375rem;
+		overflow: hidden;
+		transition: all 0.2s linear;
+      }
+
+	  .card-container:hover {
+		background-color: #f0f6ff;
+	  }
+
+	  .card-container:hover .thumbnail-img {
+		transform: scale(1.2);
+	  }
+
+      .thumbnail-box {
+        width: 100%;
+        height: 12.5rem;
+		overflow: hidden;
+      }
+
+      .thumbnail-img {
+        display: block;
+        width: 100%;
+        height: 100%;
+		transition: all 0.2s linear;
+      }
+
+      .like-btn {
+        display: block;
+        position: absolute;
+        top: 0.9375rem;
+        right: 0.9375rem;
+        cursor: pointer;
+      }
+
+	  .metadata-container {
+		position: relative;
+		height: 8.4375rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 0.9375rem 1.25rem;
+	  }
+
+	  .kebab {
+		width: 1.3125rem;
+		height: 1.0625rem;
+		position: absolute;
+		top: 0.9375rem;
+		right: 1.25rem;
+		cursor: pointer;
+	  }
+
+	  .updated-time {
+		font-size: 0.8125rem;
+		color: #666666;
+	  }
+
+	  .description-container {
+		height: 3.0625rem;
+		font-size: 1rem;
+		line-height: 1.5rem;
+		text-overflow: ellipsis;
+		overflow: hidden;
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        word-wrap:break-word; 
+	  }
+
+	  .date {
+		font-size: 0.875rem;
+		color: #333333;
+	  }
+
+	  @media only screen and (max-width: 767px) {
+		.card-container {
+		  width: 20.3125rem;
+	      height: 20.4375rem;
+		}
+		
+		.thumbnail-box {
+		  height: 12rem;
+		}
+
+		.like-btn {
+		  transform: scale(0.8825);
+		}
+	  }
+    `;
+  }
+
+  /**
+   * shadow root의 자식으로 추가될 템플릿
+   */
+  get template() {
+    return `
+	<a href=${this.prop.href}>
+      <div class="card-container">
+        <div class="thumbnail-box">
+          <img
+		    id="thumbnail"
+            class="thumbnail-img"
+            src=${this.prop.thumbnailSrc}
+            alt="thumbnail"
+          />
+          <img
+            id="like-btn"
+            class="like-btn"
+            src="/images/like-btn-unliked.svg"
+            alt="like button"
+          />
+        </div>
+        <div class="metadata-container">
+		  <img id="kebab" class="kebab" src="/images/kebab.svg" alt="kebab" />
+          <p class="updated-time">10 minutes ago</p>
+          <div id="description" class="description-container">
+            ${this.prop.metadata.description}
+          </div>
+          <p id="date" class="date">${this.prop.metadata.date}</p>
+        </div>
+      </div>
+	</a>
+    `;
+  }
+
+  handleClickLike(e) {
+    e.stopPropagation();
+    const likeBtnSrc = this.#prop.isLiked
+      ? "/images/like-btn-unliked.svg"
+      : "/images/like-btn-liked.svg";
+    this.likeBtn.setAttribute("src", likeBtnSrc);
+    this.#prop.isLiked = !this.#prop.isLiked;
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = "";
+
+    const style = document.createElement("style");
+    style.innerHTML = this.styles;
+    this.shadowRoot.appendChild(style);
+
+    const template = document.createElement("template");
+    template.innerHTML = this.template;
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.thumbnailImg = this.shadowRoot.querySelector("#thumbnail");
+    this.likeBtn = this.shadowRoot.querySelector("#like-btn");
+    this.descriptionDiv = this.shadowRoot.querySelector("#description");
+    this.dateP = this.shadowRoot.querySelector("#date");
+
+    this.likeBtn.addEventListener("click", this.handleClickLike.bind(this));
+  }
+}
+
+customElements.define("custom-link-card", LinkCard);
