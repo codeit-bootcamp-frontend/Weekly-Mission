16a84197e7d742beea2463fac8e35e2a7310fc12
Yejin Shin <shinyejin0212@gmail.com>
2023-04-16 00:39:21 +0900

    feat: card compoenet 생성

---
+++ components/card/card-component.js
@@ -0,0 +1,61 @@
+export class CardComponent extends HTMLElement {
+  constructor(imageSrc, updateTime, description, date) {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    // CSS
+    const linkElem = document.createElement("link");
+    linkElem.setAttribute("rel", "stylesheet");
+    linkElem.setAttribute("href", "card-component.css");
+    shadow.appendChild(linkElem);
+
+    // 카드 데이터
+    this._imageSrc = imageSrc;
+    this._updateTime = updateTime;
+    this._description = description;
+    this._date = date;
+
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+
+    const cardImage = document.createElement("img");
+    cardImage.classList.add("card-image");
+    cardImage.src = this._imageSrc;
+
+    const cardInfo = document.createElement("div");
+    cardInfo.classList.add("card-info");
+
+    const cardInfoHead = document.createElement("div");
+    cardInfoHead.classList.add("card-info-head");
+
+    const cardDate = document.createElement("div");
+    cardDate.classList.add("card-date");
+    cardDate.textContent = this._date;
+
+    const kebabIcon = document.createElement("img");
+    kebabIcon.classList.add("kebab-icon");
+    kebabIcon.src = "kebab.svg";
+
+    const cardDescription = document.createElement("div");
+    cardDescription.classList.add("card-description");
+    cardDescription.textContent = this._description;
+
+    const cardUpdateTime = document.createElement("div");
+    cardUpdateTime.classList.add("card-update-time");
+    cardUpdateTime.textContent = this._updateTime;
+
+    cardInfoHead.appendChild(cardDate);
+    cardInfoHead.appendChild(kebabIcon);
+
+    cardInfo.appendChild(cardInfoHead);
+    cardInfo.appendChild(cardDescription);
+    cardInfo.appendChild(cardUpdateTime);
+
+    cardContainer.appendChild(cardImage);
+    cardContainer.appendChild(cardInfo);
+
+    shadow.appendChild(cardContainer);
+  }
+}
+customElements.define("card-component", CardComponent);
