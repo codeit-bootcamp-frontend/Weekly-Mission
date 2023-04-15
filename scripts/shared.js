0f048556f12239c278c020cd514371b3dbba8891
drizzle96 <dlxkffldk37@naver.com>
2023-04-15 19:17:19 +0900

    feat: star 토글 기능 구현

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,7 +1,16 @@
 const cardContainer = document.querySelector(".card-container");
 
 function handelClickCard(e) {
-  if (e.target.className !== "card-container") {
+  const classLi = e.target.classList;
+  if (classLi.contains("star")) {
+    if (classLi.toggle("star-favor")) {
+      e.target.src = "./images/purple-star.png";
+      e.target.alt = "별(즐겨찾기)";
+    } else {
+      e.target.src = "./images/gray-star.png";
+      e.target.alt = "별(일반)";
+    }
+  } else if (!classLi.contains("card-container")) {
     window.open("https://www.codeit.kr");
   }
 }
