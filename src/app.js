2089d5f90e96d10b95152f0d82dc8a5270f31293
Seyoung Cho <seycho@cx2r8s4.42seoul.kr>
2023-04-15 19:28:47 +0900

    feat: 카드 리스트 웹 컴포넌트 제작

--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@ import { Gnb } from "./custom-elements/gnb/gnb.js";
 import { Footer } from "./custom-elements/footer/footer.js";
 import { SearchBar } from "./custom-elements/search-bar/searchBar.js";
 import { LinkCard } from "./custom-elements/link-card/linkCard.js";
+import { LinkCardList } from "./custom-elements/link-card/linkCardList.js";
