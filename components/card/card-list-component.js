1d22b0d9f5bf48ab6b7e157181190cb7c0733f4b
Yejin Shin <shinyejin0212@gmail.com>
2023-04-16 16:50:43 +0900

    refactor: connectedCallback 등록

--- components/card/card-list-component.js
+++ components/card/card-list-component.js
@@ -5,37 +5,44 @@ class CardListComponent extends HTMLElement {
     super();
 
     // Shadow DOM 생성
-    const shadow = this.attachShadow({ mode: "open" });
+    this.shadow = this.attachShadow({ mode: "open" });
 
     // CSS 파일 적용
     const linkElem = document.createElement("link");
     linkElem.setAttribute("rel", "stylesheet");
     linkElem.setAttribute("href", "/components/card/card-list-component.css");
-    shadow.appendChild(linkElem);
+    this.shadow.appendChild(linkElem);
 
     // 카드 리스트 데이터 가져오기
+    this.fetchCardsData();
+  }
+
+  connectedCallback() {
+    // 카드 컨테이너 생성
+    const cardListContainer = document.createElement("div");
+    cardListContainer.classList.add("card-list-container");
+
+    // 카드 리스트 생성
+    this.cards.forEach((card, index) => {
+      const cardComponent = new CardComponent(
+        card.imageSrc,
+        card.updateTime,
+        card.description,
+        card.date
+      );
+
+      cardListContainer.appendChild(cardComponent);
+    });
+
+    this.shadow.appendChild(cardListContainer);
+  }
+
+  fetchCardsData() {
     fetch("/static/data/card/cards.json")
       .then((response) => response.json())
       .then((data) => {
-        const cards = data.cards;
-
-        // 카드 컨테이너 생성
-        const cardListContainer = document.createElement("div");
-        cardListContainer.classList.add("card-list-container");
-
-        // 카드 리스트 생성
-        cards.forEach((card, index) => {
-          const cardComponent = new CardComponent(
-            card.imageSrc,
-            card.updateTime,
-            card.description,
-            card.date
-          );
-
-          cardListContainer.appendChild(cardComponent);
-        });
-
-        shadow.appendChild(cardListContainer);
+        this.cards = data.cards;
+        this.connectedCallback();
       })
       .catch((error) => console.log(error));
   }
