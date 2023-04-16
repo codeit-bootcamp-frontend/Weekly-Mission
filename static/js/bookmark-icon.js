3c9d112edd1bd3e11be62f1866af3f891d872f2a
Jer1s <flqmbl@gmail.com>
2023-04-16 19:47:20 +0900

    Feat: Seperate bookmark-icon module from link-card module

---
+++ static/js/bookmark-icon.js
@@ -0,0 +1,54 @@
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>
+  .star {
+    cursor: pointer;
+    position: absolute;
+    top: 1.6rem;
+    right: 1.6rem;
+    width: 3.2rem;
+  }
+    
+  .star #marked {
+    display: none;
+  }
+
+  </style>
+  <img id="unmarked" class="star" src="/static/star1.png" alt="Unmarked icon">
+  <img id="marked" class="star" src="/static/star2.png" alt="Marked icon">
+`;
+
+class Bookmark extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.append(template.content.cloneNode(true));
+    
+    const bookmark = this.getAttribute("bookmark");
+    const marked = this.shadowRoot.getElementById("marked");
+    const unmarked = this.shadowRoot.getElementById("unmarked");
+
+    if (bookmark === "true") {
+      marked.style.display = "block";
+      unmarked.style.display = "none";
+    } else {
+      marked.style.display = "none";
+      unmarked.style.display = "block";
+    }
+
+    function bookmarkToggle(e) {
+      if (marked.style.display === "none") {
+        marked.style.display = "block";
+        unmarked.style.display = "none";
+      } else {
+        marked.style.display = "none";
+        unmarked.style.display = "block";
+      }
+      e.stopPropagation();
+    }
+
+    marked.addEventListener('click', bookmarkToggle);
+    unmarked.addEventListener('click', bookmarkToggle);
+  }
+}
+
+customElements.define("bookmark-icon", Bookmark);
--- static/js/link-card.js
+++ static/js/link-card.js
@@ -21,17 +21,6 @@ template.innerHTML = `
       box-shadow: 0 0.5rem 2.5rem rgba(0, 0, 0, 0.08);
     }
 
-    #card .star {
-      position: absolute;
-      top: 1.6rem;
-      right: 1.6rem;
-      width: 3.2rem;
-    }
-    
-    .star #marked {
-      display: none;
-    }
-
     #card #card-image {
       background-repeat: no-repeat;
       background-position: center;
@@ -90,10 +79,7 @@ template.innerHTML = `
     }
   </style>
   <div id="card">
-    <div id="card-image">
-      <img id="unmarked" class="star" src="/static/star1.png" alt="Unmarked icon">
-      <img id="marked" class="star" src="/static/star2.png" alt="Marked icon">
-    </div>
+    <div id="card-image"></div>
     <div id="card-text">
       <div id="first-line">
         <p id="since">10분 전</span>
@@ -116,40 +102,13 @@ class Card extends HTMLElement {
     this.shadowRoot.append(template.content.cloneNode(true));
     
     const backgroundImg = this.getAttribute("image");
-    const bookmark = this.getAttribute("bookmark");
-    const card = this.shadowRoot.getElementById("card");
-    const marked = this.shadowRoot.getElementById("marked");
-    const unmarked = this.shadowRoot.getElementById("unmarked");
-
     this.shadowRoot.getElementById("card-image").style.backgroundImage = `url(${backgroundImg})`;
     this.shadowRoot.getElementById("since").innerText = this.getAttribute("since");
     this.shadowRoot.getElementById("description").innerText = this.getAttribute("description");
     this.shadowRoot.getElementById("date").innerText = this.getAttribute("date");
 
-
+    const card = this.shadowRoot.getElementById("card");
     card.addEventListener('click', () => window.open('https://www.codeit.kr'));
-    if (bookmark === "true") {
-      marked.style.display = "block";
-      unmarked.style.display = "none";
-    } else {
-      marked.style.display = "none";
-      unmarked.style.display = "block";
-    }
-
-    function bookmarkToggle(e) {
-      if (marked.style.display === "none") {
-        marked.style.display = "block";
-        unmarked.style.display = "none";
-      } else {
-        marked.style.display = "none";
-        unmarked.style.display = "block";
-      }
-      e.stopPropagation();
-    }
-
-    marked.addEventListener('click', bookmarkToggle);
-    unmarked.addEventListener('click', bookmarkToggle);
-
   }
 }
 
