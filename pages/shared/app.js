10d41dd5d47e78f8a65b0075796f332c4a58ffe3
wooleejaan <wooleejaan@gmail.com>
2023-04-18 16:02:38 +0900

    refactor(jadestern): 의미 없는 div.inner 요소들 제거

--- pages/shared/app.js
+++ pages/shared/app.js
@@ -6,12 +6,11 @@ import { getElem } from "/hooks/getElement.js";
 
 (function () {
   const cardContainer = getElem(document, "main .contents .card-container");
-  const cardContainerInner = getElem(cardContainer, ".inner");
 
   const init = () => {
     window.addEventListener("DOMContentLoaded", () => {
       for (let i = 0; i < 9; i++) {
-        cardContainerInner.append(new Card(i + 1));
+        cardContainer.append(new Card(i + 1));
       }
     });
   };
