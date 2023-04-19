ccd2f2a69cadbcf7dbedf21cdea332b5a4627a2b
hayoung-99 <nhy990130@gmail.com>
2023-04-19 22:28:10 +0900

    temp: 로그인 유저 데이터 dummy 생성

---
+++ data/user.js
@@ -0,0 +1,8 @@
+const user = {
+  id: 1,
+  name: "코드잇",
+  email: "codeit@codeit.com",
+  profileImageSource: "https://codeit-front.s3.ap-northeast-2.amazonaws.com/images/default_profile.png",
+};
+
+export default user;
