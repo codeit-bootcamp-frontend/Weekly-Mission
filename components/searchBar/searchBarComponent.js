25ab06bf03f658d8fae641c57fd8856285d32f0d
Yejin Shin <shinyejin0212@gmail.com>
2023-04-19 11:32:59 +0900

    refactor: seachBar 컴포넌트 DOM 노드 create 분리

--- components/searchBar/searchBarComponent.js
+++ components/searchBar/searchBarComponent.js
@@ -1,36 +1,57 @@
 class SearchBarComponent extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    // Shadow DOM 생성
-    const shadow = this.attachShadow({ mode: "open" });
+  connectedCallback() {
+    this.render();
+  }
 
-    // CSS 파일 적용
+  createLinkElem() {
     const linkElem = document.createElement("link");
     linkElem.setAttribute("rel", "stylesheet");
     linkElem.setAttribute(
       "href",
       "/components/searchBar/search-bar-component.css"
     );
-    shadow.appendChild(linkElem);
+    return linkElem;
+  }
 
+  createSearchBarContainer() {
     const searchBarContainer = document.createElement("div");
     searchBarContainer.classList.add("search-wrap");
+    const logoImage = this.createLogoImage();
+    const searchBarInput = this.createSearchBarInput();
 
+    searchBarContainer.appendChild(logoImage);
+    searchBarContainer.appendChild(searchBarInput);
+
+    return searchBarContainer;
+  }
+
+  createLogoImage() {
     const logoImage = document.createElement("img");
     logoImage.classList.add("search-lens-icon");
     logoImage.alt = "search-lens-icon";
     logoImage.src = "/static/imgs/search-bar-lens-icon.svg";
+    return logoImage;
+  }
 
+  createSearchBarInput() {
     const searchBarInput = document.createElement("input");
     searchBarInput.classList.add("search-bar-input");
     searchBarInput.placeholder = "원하는 링크를 검색해 보세요";
+    return searchBarInput;
+  }
 
-    // 자식 추가
-    searchBarContainer.appendChild(logoImage);
-    searchBarContainer.appendChild(searchBarInput);
+  render() {
+    const linkElem = this.createLinkElem();
+    const searchBarContainer = this.createSearchBarContainer();
 
-    shadow.appendChild(searchBarContainer);
+    this.shadowRoot.appendChild(linkElem);
+    this.shadowRoot.appendChild(searchBarContainer);
   }
 }
+
 customElements.define("search-bar", SearchBarComponent);
