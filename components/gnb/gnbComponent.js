bfeac8187954de0ae6a8b84d8e80aecf134de7be
Yejin Shin <shinyejin0212@gmail.com>
2023-04-20 15:12:59 +0900

    refactor: gnb Component DOM 구성 template으로  변경

--- components/gnb/gnbComponent.js
+++ components/gnb/gnbComponent.js
@@ -1,8 +1,8 @@
 class GnbComponent extends HTMLElement {
   #prop = null;
+
   constructor() {
     super();
-
     this.attachShadow({ mode: "open" });
   }
 
@@ -16,39 +16,33 @@ class GnbComponent extends HTMLElement {
 
   set prop(newProp) {
     this.#prop = newProp;
-    this.gnbContainer.removeChild(this.loginButton);
-
-    const loginButton = this.checkLoginStatus();
-    this.loginButton = loginButton;
-
-    this.gnbContainer.appendChild(loginButton);
-
-    this.shadowRoot.appendChild(this.gnbContainer);
+    this.gnbContainer.innerHTML = this.template;
   }
 
-  checkLoginStatus() {
-    const loginStatusElement = this.prop
-      ? this.createLoggedInText()
-      : this.createLoginButton();
-    return loginStatusElement;
+  get loginContent() {
+    return this.prop ? this.loggedInText : this.loginButton;
   }
 
-  createLoginButton() {
-    const loginButton = document.createElement("a");
-    loginButton.classList.add("login");
-    loginButton.href = "/signin/";
-    loginButton.textContent = "로그인";
-    return loginButton;
+  get loginButton() {
+    return `<a class="login" href="/signin/">로그인</a>`;
   }
 
-  createLoggedInText() {
-    const loggedInText = document.createElement("div");
-    loggedInText.classList.add("user-profile");
-    loggedInText.innerHTML = `
-      <img class="profile-icon" src="${this.prop.profileSrc}"/>
-      <p class="user-email">${this.prop.email}</p>
+  get loggedInText() {
+    return `
+      <div class="user-profile">
+        <img class="profile-icon" src="${this.prop.profileSrc}" />
+        <p class="user-email">${this.prop.email}</p>
+      </div>
     `;
-    return loggedInText;
+  }
+
+  get template() {
+    return `
+      <a href="/">
+        <img class="logo" alt="logo" src="/static/imgs/Linkbrary.svg" />
+      </a>
+      ${this.loginContent}
+    `;
   }
 
   render() {
@@ -60,21 +54,8 @@ class GnbComponent extends HTMLElement {
     const gnbContainer = document.createElement("nav");
     gnbContainer.classList.add("gnb-container");
     this.gnbContainer = gnbContainer;
-    const logoLinkAnchor = document.createElement("a");
-    logoLinkAnchor.href = "/";
 
-    const logoImage = document.createElement("img");
-    logoImage.classList.add("logo");
-    logoImage.alt = "logo";
-    logoImage.src = "/static/imgs/Linkbrary.svg";
-
-    const loginButton = this.checkLoginStatus();
-    this.loginButton = loginButton;
-    logoLinkAnchor.appendChild(logoImage);
-
-    gnbContainer.appendChild(logoLinkAnchor);
-    gnbContainer.appendChild(loginButton);
-
+    gnbContainer.innerHTML = this.template;
     this.shadowRoot.appendChild(gnbContainer);
   }
 }
