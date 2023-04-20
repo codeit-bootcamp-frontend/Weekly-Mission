6ba8ce4f51a167c9fe6436dd04ee668af470b9fa
hayoung-99 <nhy990130@gmail.com>
2023-04-20 15:49:50 +0900

    feat: folder 데이터를 Header에 적용

--- data/folder.js
+++ data/folder.js
@@ -2,7 +2,6 @@ async function getFolderData() {
   try {
     const response = await fetch("https://bootcamp-api.codeit.kr/api/sample/folder");
     const result = await response.json();
-    console.log(result);
     return result;
   } catch {
     console.log("잘못된 url이거나 json 데이터가 아닙니다.");
--- shared/index.js
+++ shared/index.js
@@ -1,4 +1,5 @@
 import getUserData from "/data/user.js";
+import getFolderData from "/data/folder.js";
 
 async function renderGNB() {
   try {
@@ -10,4 +11,29 @@ async function renderGNB() {
   }
 }
 
+async function renderHeader() {
+  try {
+    const { data } = await getFolderData();
+    const { folder } = data;
+    const { name } = folder;
+    const { id, name: ownerName, profileImageSource } = folder.owner;
+
+    const header = document.querySelector("header");
+    header.dataset.pid = id;
+
+    const headerTitle = document.querySelector(".favorites");
+    headerTitle.textContent = name;
+
+    const profileImage = document.querySelector(".user-avatar");
+    profileImage.src = profileImageSource;
+
+    const profileName = document.querySelector(".user-nickname");
+    profileName.textContent = `@${ownerName}`;
+  } catch {
+    console.log("folder의 데이터가 존재하지 않습니다.");
+  }
+}
+
+//호출
 renderGNB();
+renderHeader();
