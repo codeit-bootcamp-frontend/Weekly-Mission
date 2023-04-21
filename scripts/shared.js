773aeb7a023b0733ee37256a2d043fd89adbb244
drizzle96 <dlxkffldk37@naver.com>
2023-04-21 17:10:59 +0900

    feat: shared 페이지 header 부분 api 구현

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,17 +1,33 @@
 import ProcessFolderData from "/scripts/ProcessFolderData.js";
 
 const cardContainer = document.querySelector(".card-container");
-
-// 2. card 갯수만큼 card 반복 생성
-async function createCard() {
+// 1. data fetch
+async function getData() {
   const processorData = await new ProcessFolderData();
-  const folderData = await processorData.data;
-  console.log(folderData.count);
+  return await processorData.data;
+}
+// 2. header 내용 넣어주기
+async function applyHeaderData() {
+  const folderData = await getData();
 
+  const avatarTag = document.querySelector(".avatar-tag");
+  const avatar = document.querySelector(".avatar"); //이미지
+  const headerTitle = document.querySelector(".header-title");
+
+  console.log(folderData.owner.profileImageSource);
+
+  avatarTag.textContent = folderData.owner.name;
+  avatar.setAttribute("src", folderData.owner.profileImageSource);
+  headerTitle.textContent = folderData.name;
+}
+applyHeaderData();
+
+// 3. card 갯수만큼 card 반복 생성
+async function createCard() {
+  const folderData = await getData();
   for (let i = 0; i < folderData.count; i++) {
     const card = document.createElement("custom-card");
     cardContainer.appendChild(card);
   }
 }
-
 createCard();
