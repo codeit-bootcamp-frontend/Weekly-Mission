604b52cdc76d48344141e2510ee59554418c55a2
Jer1s <flqmbl@gmail.com>
2023-04-21 17:21:42 +0900

    Style: 공백

--- static/js/shared.js
+++ static/js/shared.js
@@ -25,59 +25,59 @@ async function getFolder() {
   }
 
   links.forEach((link) => {
-  const { imageSource, description, createdAt, url } = link
-  const linkCardElement = document.createElement("link-card")
-  
-  if (imageSource !== undefined) {
-    linkCardElement.setAttribute("imageSource", imageSource)
-  } else {
-    linkCardElement.setAttribute("imageSource", "/static/img/default-image-source.png")
-  }
-
-  if (description !== undefined) {
-    linkCardElement.setAttribute("description", description)
-  }
-
-  if (url !== undefined) {
-    linkCardElement.setAttribute("card-url", url)
-  }
-
-  if (createdAt !== undefined) {
-    const createdDate = new Date(createdAt);
-    const nowDate = new Date()
-    const timeDiff = nowDate - createdDate;
-    const minuteDiff = parseInt(timeDiff / 1000 / 60);
-    const hourDiff = parseInt(timeDiff / 1000 / 60 / 60);
-    const dateDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24);
-    const monthDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24 / 30);
-    const yearDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24 / 365);
-    let since = "";
-    if (minuteDiff < 2) {
-      since = `1 minute ago`;
-    } else if (minuteDiff < 60) {
-      since = `${minuteDiff} minutes ago`
-    } else if (hourDiff < 2) {
-      since = "1 hour ago";
-    } else if (hourDiff < 24) {
-      since = `${hourDiff} hours ago`
-    } else if (dateDiff < 2) {
-      since = "1 day ago";
-    } else if (dateDiff < 31) {
-      since = `${dateDiff} days ago`
-    } else if (monthDiff < 2) {
-      since = "1 month ago";
-    } else if (monthDiff < 12) {
-      since = `${monthDiff} months ago`
-    } else if (yearDiff < 2) {
-      since = "1 year ago";
-    } else if (yearDiff >= 2) {
-      since = `${yearDiff} months ago`
+    const { imageSource, description, createdAt, url } = link
+    const linkCardElement = document.createElement("link-card")
+    
+    if (imageSource !== undefined) {
+      linkCardElement.setAttribute("imageSource", imageSource)
+    } else {
+      linkCardElement.setAttribute("imageSource", "/static/img/default-image-source.png")
     }
-    linkCardElement.setAttribute("createdAt", Intl.DateTimeFormat().format(createdDate))
-    linkCardElement.setAttribute("since", since)
-  }
 
-  cardContainer.appendChild(linkCardElement)
+    if (description !== undefined) {
+      linkCardElement.setAttribute("description", description)
+    }
+
+    if (url !== undefined) {
+      linkCardElement.setAttribute("card-url", url)
+    }
+
+    if (createdAt !== undefined) {
+      const createdDate = new Date(createdAt);
+      const nowDate = new Date()
+      const timeDiff = nowDate - createdDate;
+      const minuteDiff = parseInt(timeDiff / 1000 / 60);
+      const hourDiff = parseInt(timeDiff / 1000 / 60 / 60);
+      const dateDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24);
+      const monthDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24 / 30);
+      const yearDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24 / 365);
+      let since = "";
+      if (minuteDiff < 2) {
+        since = `1 minute ago`;
+      } else if (minuteDiff < 60) {
+        since = `${minuteDiff} minutes ago`
+      } else if (hourDiff < 2) {
+        since = "1 hour ago";
+      } else if (hourDiff < 24) {
+        since = `${hourDiff} hours ago`
+      } else if (dateDiff < 2) {
+        since = "1 day ago";
+      } else if (dateDiff < 31) {
+        since = `${dateDiff} days ago`
+      } else if (monthDiff < 2) {
+        since = "1 month ago";
+      } else if (monthDiff < 12) {
+        since = `${monthDiff} months ago`
+      } else if (yearDiff < 2) {
+        since = "1 year ago";
+      } else if (yearDiff >= 2) {
+        since = `${yearDiff} months ago`
+      }
+      linkCardElement.setAttribute("createdAt", Intl.DateTimeFormat().format(createdDate))
+      linkCardElement.setAttribute("since", since)
+    }
+
+    cardContainer.appendChild(linkCardElement)
   })
 }
 
