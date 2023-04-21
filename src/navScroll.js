d159cd1ef75d855d9fee3d963986a7b4cf51abe1
Seyoung Cho <seycho@cx2r8s4.42seoul.kr>
2023-04-21 21:19:16 +0900

    refactor(mentor): gnb 높이 계산 방식 수정
    - 높이 저장 변수를 스크롤 핸들러 외부로 이동
    - 높이 계산방법 offsetHeight 프로퍼티로 대체

--- src/navScroll.js
+++ src/navScroll.js
@@ -2,18 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   let prevScrollpos = window.scrollY;
   const navElem = document.querySelector("#nav-wrapper");
   const headerElem = document.querySelector("header");
+  let navHeight = navElem.offsetHeight;
 
   window.addEventListener("scroll", () => {
-    let navHeight = window
-      .getComputedStyle(navElem, null)
-      .getPropertyValue("height");
-
     let currentScrollPos = window.scrollY;
     if (prevScrollpos > currentScrollPos) {
       headerElem.style.top = "0";
       headerElem.classList.add("shadow");
     } else {
-      headerElem.style.top = "-" + navHeight;
+      headerElem.style.top = "-" + navHeight + "px";
       headerElem.classList.remove("shadow");
     }
 
