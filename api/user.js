a4c0907cda7deafde3662a14e39e19dfd6712747
Noahcodeit <dowongim021@gmail.com>
2023-04-22 21:33:42 +0900

    feat: user api데이터 불러오기

---
+++ api/user.js
@@ -0,0 +1,15 @@
+async function getUserData() {
+  try {
+    const response = await fetch(
+      "https://bootcamp-api.codeit.kr/api/sample/user"
+    );
+    const result = await response.json();
+    return result;
+  } catch {
+    console.log("잘못된 url이거나 json데이터가 아닙니다.");
+    const result = null;
+    return result;
+  }
+}
+
+export default getUserData;
