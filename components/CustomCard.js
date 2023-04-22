1ee590e655b3b83e7c2edcd9df4cdd8c526a7033
drizzle96 <dlxkffldk37@naver.com>
2023-04-22 12:22:37 +0900

    feat: .card time 내용에 Date 연산 구현

--- components/CustomCard.js
+++ components/CustomCard.js
@@ -35,6 +35,12 @@ class CustomCard extends HTMLElement {
       ? cardData.imageSource
       : "/images/logo.png";
 
+    const createdDate = Intl.DateTimeFormat("kr").format(
+      new Date(cardData.createdAt)
+    );
+
+    const passedTime = this.calculatePassedTime(new Date(cardData.createdAt));
+
     this.shadowRoot.innerHTML = `
       <style>
         @import url("/components/styles/CustomCard.css");
@@ -49,16 +55,49 @@ class CustomCard extends HTMLElement {
             <custom-star></custom-star>
           </div>
           <div class="content-container">
-            <div class="publish-time">${cardData.createdAt}</div>
+            <div class="passed-time">${passedTime}</div>
             <button class="btn-overflow-menu"></button>
             <h2 class="content">
               ${cardData.description}
             </h2>
-            <div class="publish-date">${cardData.createdAt}</div>
+            <div class="created-date">${createdDate}</div>
           </div>
         </a>
       </article>
     `;
+  }
+
+  calculatePassedTime(createdTime) {
+    const MINUTE = 60 * 1000;
+    const HOUR = 60 * MINUTE;
+    const DAY = 24 * HOUR;
+    const MONTH = 30 * DAY;
+    const YEAR = 12 * MONTH;
+
+    const currentTime = new Date();
+    const timeDiff = currentTime - createdTime;
+
+    if (timeDiff < 2 * MINUTE) {
+      return "1 minute ago";
+    } else if (timeDiff <= 59 * MINUTE) {
+      return `${Math.floor(timeDiff / MINUTE)} minutes ago`;
+    } else if (timeDiff <= HOUR + 59 * MINUTE) {
+      return "1 hour ago";
+    } else if (timeDiff <= 23 * HOUR) {
+      return `${Math.floor(timeDiff / HOUR)} hours ago`;
+    } else if (timeDiff <= DAY + 23 * HOUR) {
+      return "1 day ago";
+    } else if (timeDiff <= 30 * DAY) {
+      return `${Math.floor(timeDiff / DAY)} days ago`;
+    } else if (timeDiff <= 31 * DAY + 12 * HOUR) {
+      return "1 month ago";
+    } else if (timeDiff <= 11 * MONTH) {
+      return `${Math.floor(timeDiff / MONTH)} months ago`;
+    } else if (timeDiff <= 12 * MONTH + 15 * DAY) {
+      return "1 year ago";
+    } else {
+      return `${Math.floor(timeDiff / YEAR)} years ago`;
+    }
   }
 }
 
