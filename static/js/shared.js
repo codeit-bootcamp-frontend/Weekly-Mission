4befc70090c554661c027f482990df52e31beec6
Jer1s <flqmbl@gmail.com>
2023-04-27 01:25:05 +0900

    Refactor: elif문 switch문으로 변경, fetch 함수에 try-catch문 적용

--- static/js/shared.js
+++ static/js/shared.js
@@ -1,84 +1,102 @@
-const cardContainer = document.querySelector('.card-container');
-const userAvatar = document.querySelector('.user-avatar');
-const username = document.querySelector('.username');
-const bookmarkTitle = document.querySelector('.bookmark-title');
+const cardContainer = document.querySelector(".card-container");
+const userAvatar = document.querySelector(".user-avatar");
+const username = document.querySelector(".username");
+const bookmarkTitle = document.querySelector(".bookmark-title");
 
 async function getFolder() {
-  const response = await fetch(
-    "https://bootcamp-api.codeit.kr/api/sample/folder"
-  );
-  const { data }  = await response.json();
-  const { folder } = data
-  const { name, owner, links } = folder
+  try {
+    const response = await fetch(
+      "https://bootcamp-api.codeit.kr/api/sample/folder"
+    );
+    const { data } = await response.json();
+    const { folder } = data;
+    const { name, owner, links } = folder;
 
-  if (name !== undefined) {
-    bookmarkTitle.innerText = name
-  }
-  if (owner.profileImageSource !== undefined) {
-    userAvatar.src = owner.profileImageSource
-  } else {
-    userAvatar.src = "/static/img/default-avatar.png"
-  }
-
-  if (owner.name !== undefined) {
-    username.innerText = `@${owner.name}`
-  }
-
-  links.forEach((link) => {
-    const { imageSource, description, createdAt, url } = link
-    const linkCardElement = document.createElement("link-card")
-    
-    if (imageSource !== undefined) {
-      linkCardElement.setAttribute("imageSource", imageSource)
+    if (name !== undefined) {
+      bookmarkTitle.innerText = name;
+    }
+    if (owner.profileImageSource !== undefined) {
+      userAvatar.src = owner.profileImageSource;
     } else {
-      linkCardElement.setAttribute("imageSource", "/static/img/default-image-source.png")
+      userAvatar.src = "/static/img/default-avatar.png";
+    }
+    if (owner.name !== undefined) {
+      username.innerText = `@${owner.name}`;
     }
 
-    if (description !== undefined) {
-      linkCardElement.setAttribute("description", description)
-    }
+    links.forEach((link) => {
+      const { imageSource, description, createdAt, url } = link;
+      const linkCardElement = document.createElement("link-card");
 
-    if (url !== undefined) {
-      linkCardElement.setAttribute("card-url", url)
-    }
-
-    if (createdAt !== undefined) {
-      const createdDate = new Date(createdAt);
-      const nowDate = new Date()
-      const timeDiff = nowDate - createdDate;
-      const minuteDiff = parseInt(timeDiff / 1000 / 60);
-      const hourDiff = parseInt(timeDiff / 1000 / 60 / 60);
-      const dateDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24);
-      const monthDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24 / 30);
-      const yearDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24 / 365);
-      let since = "";
-      if (minuteDiff < 2) {
-        since = `1 minute ago`;
-      } else if (minuteDiff < 60) {
-        since = `${minuteDiff} minutes ago`
-      } else if (hourDiff < 2) {
-        since = "1 hour ago";
-      } else if (hourDiff < 24) {
-        since = `${hourDiff} hours ago`
-      } else if (dateDiff < 2) {
-        since = "1 day ago";
-      } else if (dateDiff < 31) {
-        since = `${dateDiff} days ago`
-      } else if (monthDiff < 2) {
-        since = "1 month ago";
-      } else if (monthDiff < 12) {
-        since = `${monthDiff} months ago`
-      } else if (yearDiff < 2) {
-        since = "1 year ago";
-      } else if (yearDiff >= 2) {
-        since = `${yearDiff} months ago`
+      if (imageSource !== undefined) {
+        linkCardElement.setAttribute("imageSource", imageSource);
+      } else {
+        linkCardElement.setAttribute(
+          "imageSource",
+          "/static/img/default-image-source.png"
+        );
       }
-      linkCardElement.setAttribute("createdAt", Intl.DateTimeFormat().format(createdDate))
-      linkCardElement.setAttribute("since", since)
-    }
-
-    cardContainer.appendChild(linkCardElement)
-  })
+      if (description !== undefined) {
+        linkCardElement.setAttribute("description", description);
+      }
+      if (url !== undefined) {
+        linkCardElement.setAttribute("card-url", url);
+      }
+      if (createdAt !== undefined) {
+        const createdDate = new Date(createdAt);
+        const nowDate = new Date();
+        const timeDiff = nowDate - createdDate;
+        const minuteDiff = parseInt(timeDiff / 1000 / 60);
+        const hourDiff = parseInt(timeDiff / 1000 / 60 / 60);
+        const dateDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24);
+        const monthDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24 / 30);
+        const yearDiff = parseInt(timeDiff / 1000 / 60 / 60 / 24 / 365);
+        let since = "";
+        switch (true) {
+          case minuteDiff < 2:
+            since = `1 minute ago`;
+            break;
+          case minuteDiff < 60:
+            since = `${minuteDiff} minutes ago`;
+            break;
+          case hourDiff < 2:
+            since = "1 hour ago";
+            break;
+          case hourDiff < 24:
+            since = `${hourDiff} hours ago`;
+            break;
+          case dateDiff < 2:
+            since = "1 day ago";
+            break;
+          case dateDiff < 31:
+            since = `${dateDiff} days ago`;
+            break;
+          case monthDiff < 2:
+            since = "1 month ago";
+            break;
+          case monthDiff < 12:
+            since = `${monthDiff} months ago`;
+            break;
+          case yearDiff < 2:
+            since = "1 year ago";
+            break;
+          case yearDiff >= 2:
+            since = `${yearDiff} months ago`;
+            break;
+          default:
+            break;
+        }
+        linkCardElement.setAttribute(
+          "createdAt",
+          Intl.DateTimeFormat().format(createdDate)
+        );
+        linkCardElement.setAttribute("since", since);
+      }
+      cardContainer.appendChild(linkCardElement);
+    });
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
 }
 
 getFolder();
