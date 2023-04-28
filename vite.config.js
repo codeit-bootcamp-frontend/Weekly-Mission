9c826c6e4253ebd9f29458b3b89b5780e6adf5d5
Jer1s <flqmbl@gmail.com>
2023-04-29 02:37:19 +0900

    Chore: Move api file to utils

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     extensions: [".js", ".jsx"],
     alias: {
       "@": path.resolve(__dirname, "src"),
-      api: path.resolve(__dirname, "src/api"),
       assets: path.resolve(__dirname, "src/assets"),
       components: path.resolve(__dirname, "src/components"),
       pages: path.resolve(__dirname, "src/pages"),
