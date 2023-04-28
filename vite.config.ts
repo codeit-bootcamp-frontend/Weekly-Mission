02ce82bff69709cf09d6df0e4b643f10cacb0573
SeyoungCho <wnl383@naver.com>
2023-04-28 10:44:25 +0900

    chore: docker-compose파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 8000, // 이렇게 설정해두면 로컬에서 5173포트가 아닌 8000포트로 접속하면 된다.
+    //도커에서는 5174포트로 접속이 가능해진다.
+  },
+});
