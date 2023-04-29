bc6a976c9d48a8370799f0e3d48f174719f405e6
hayoung-99 <nhy990130@gmail.com>
2023-04-29 21:59:13 +0900

    add: api 데이터 요청

---
+++ api/folderData.js
@@ -0,0 +1,13 @@
+async function getFolderData() {
+  try {
+    const response = await fetch("https://bootcamp-api.codeit.kr/api/sample/folder");
+    const result = await response.json();
+    return result;
+  } catch {
+    console.log("잘못된 url이거나 json 데이터가 아닙니다.");
+    const result = null;
+    return result;
+  }
+}
+
+export default getFolderData;
