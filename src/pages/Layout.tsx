8743b6661918d821a4a20e4dae16daf0ecf1b1eb
Seyoung Cho <seycho@c10r3s10.42seoul.kr>
2023-04-29 22:07:31 +0900

    feat: Layout에서 현재 path가 /shared 일 때만 user api호출하게 조치
    - 해당 조치 없이는 현재로서는 로그인 버튼을 볼 수 있는 방법이 없습니다.
    - 추후 로그인 인증이 구현되고 되돌리도록 합니다.

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,26 +1,29 @@
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { getUserRequest } from "../api/userApi";
 import Footer from "../components/Footer/Footer";
 import Gnb, { GnbProps } from "../components/Gnb/Gnb";
 
 const Layout = () => {
   const [userData, setUserData] = useState<GnbProps>();
+  const location = useLocation();
 
   useEffect(() => {
-    getUserRequest()
-      .then((res) => res.data)
-      .then((data) => {
-        const {
-          name: username,
-          email,
-          profileImageSource: profileImgSrc,
-        } = data;
-        setUserData({ username, email, profileImgSrc });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    if (location.pathname === "/shared") {
+      getUserRequest()
+        .then((res) => res.data)
+        .then((data) => {
+          const {
+            name: username,
+            email,
+            profileImageSource: profileImgSrc,
+          } = data;
+          setUserData({ username, email, profileImgSrc });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
   }, []);
 
   return (
