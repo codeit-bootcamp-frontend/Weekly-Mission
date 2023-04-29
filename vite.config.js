923d5638a349d7f9f54ee96b9e2ae6219f8f116b
Yejin Shin <shinyejin0212@gmail.com>
2023-04-29 19:59:10 +0900

    feat: react 기본 설정
    - vite로 react 프로젝트 생성
    - vite.config.js에 상대경로 alias
    - CRLF 줄 끝 처리

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "src") },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
+    ],
+  },
+});
