9315d69ff833732be53a872839a6842ab5dcd777
Yejin Shin <shinyejin0212@gmail.com>
2023-04-30 23:10:15 +0900

    feat: useFetchData Custom Hooks 구현

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function useFetchData(url) {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+}
+
+export default useFetchData;
