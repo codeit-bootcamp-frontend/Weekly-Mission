0e0acc8f645295fd00cbe73561805a0179170352
SeyoungCho <wnl383@naver.com>
2023-04-30 22:48:56 +0900

    ccc

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import NotFoundPage from "./pages/NotFoundPage";
 import SharedPage from "./pages/SharedPage";
 import SiginPage from "./pages/SiginPage";
 import SignupPage from "./pages/SignupPage";
+import React from "react";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<HomePage />} />
-          <Route path="shared" element={<SharedPage />} />
-        </Route>
-        <Route path="/signin" element={<SiginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<HomePage />} />
+            <Route path="shared" element={<SharedPage />} />
+          </Route>
+          <Route path="/signin" element={<SiginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
