ef721c3c5da8ad1451d8b311af710029ba1bd065
Jer1s <flqmbl@gmail.com>
2023-04-30 20:13:10 +0900

    Refactor: Change function declaration to arrow function of api functions

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,20 +2,22 @@ import axios from "axios";
 
 const BASE_URL = "https://bootcamp-api.codeit.kr/api";
 
-export async function getFolders() {
+export const getFolders = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/sample/folder`);
     return response.data;
   } catch (error) {
     console.log(error);
+    return;
   }
-}
+};
 
-export async function getUsers() {
+export const getUsers = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/sample/user`);
     return response.data;
   } catch (error) {
     console.log(error);
+    return;
   }
-}
+};
