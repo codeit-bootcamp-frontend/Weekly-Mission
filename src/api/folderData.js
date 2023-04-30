6eb56000681e7878a6749f331e73b71fb50bdd0a
hayoung-99 <nhy990130@gmail.com>
2023-04-30 20:04:01 +0900

    move: api 요청 src 폴더로 이동

--- src/api/folderData.js
+++ src/api/folderData.js
@@ -1,12 +1,11 @@
 async function getFolderData() {
   try {
     const response = await fetch("https://bootcamp-api.codeit.kr/api/sample/folder");
-    const result = await response.json();
-    return result;
+    const { data } = await response.json();
+    return data;
   } catch {
     console.log("잘못된 url이거나 json 데이터가 아닙니다.");
-    const result = null;
-    return result;
+    return null;
   }
 }
 
