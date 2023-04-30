17219f9e5769ef97363d286e19c6986f7bbf5438
hayoung-99 <nhy990130@gmail.com>
2023-04-30 20:37:53 +0900

    add: common css 추가

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,8 @@ import defaultImg from "../images/default_image.png";
 import kebabImg from "../images/kebab.svg";
 import defaultStar from "../images/default-star.svg";
 import selectedStar from "../images/selected-star.svg";
+import "../common/color.css";
+import "../common/reset.css";
 import "./Card.css";
 
 const DEFAULT = {
