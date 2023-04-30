024474ddb4edeb2c64895edd9b7eabe8e0215800
Yejin Shin <shinyejin0212@gmail.com>
2023-04-30 17:45:08 +0900

    feat: ContextAPI로 user 정보 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DefaultLayout from "@/layouts/DefaultLayout";
+import HomePage from "@/pages/HomePage";
+import SharedPage from "@/pages/SharedPage";
+
+//style
+import GlobalStyle from "@/styles/GlobalStyles";
+
+// contextAPI
+import { UserProvider } from "@/contexts/UserContext";
 
 function App() {
-  return <></>;
+  return (
+    <UserProvider>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/shared" element={<SharedPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </UserProvider>
+  );
 }
 
 export default App;
