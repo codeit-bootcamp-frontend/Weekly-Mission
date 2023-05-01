48eedfdf0d2fa2509f8a23d98eca013a5de85c7c
sally <9609sally@gmail.com>
2023-05-02 01:41:55 +0900

    style: 빈 태그 삭제, 겹치는 style 삭제

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,35 +6,33 @@ import instagram from "../assets/images/sns-icons/instagram.png";
 
 function Footer() {
   return (
-    <>
-      <div className="footer-container">
-        <footer>
-          <div id="copyright">©codeit-2023</div>
-          <div className="bottomLinks">
-            <a className="bottomLink privacy-policy" href="/privacy">
-              Privacy Policy
-            </a>
-            <a className="bottomLink" href="/faq">
-              FAQ
-            </a>
-          </div>
-          <div className="snsIcons">
-            <a href="https://www.facebook.com">
-              <img className="snsIcon" src={facebook} alt="페이스북" />
-            </a>
-            <a href="https://twitter.com">
-              <img className="snsIcon" src={twitter} alt="트위터" />
-            </a>
-            <a href="https://www.youtube.com">
-              <img className="snsIcon" src={youtube} alt="유튜브" />
-            </a>
-            <a href="https://www.instagram.com">
-              <img className="snsIcon" src={instagram} alt="인스타그램" />
-            </a>
-          </div>
-        </footer>
-      </div>
-    </>
+    <div className="footer-container">
+      <footer>
+        <div id="copyright">©codeit-2023</div>
+        <div className="bottomLinks">
+          <a className="bottomLink privacy-policy" href="/privacy">
+            Privacy Policy
+          </a>
+          <a className="bottomLink" href="/faq">
+            FAQ
+          </a>
+        </div>
+        <div className="snsIcons">
+          <a href="https://www.facebook.com">
+            <img className="snsIcon" src={facebook} alt="페이스북" />
+          </a>
+          <a href="https://twitter.com">
+            <img className="snsIcon" src={twitter} alt="트위터" />
+          </a>
+          <a href="https://www.youtube.com">
+            <img className="snsIcon" src={youtube} alt="유튜브" />
+          </a>
+          <a href="https://www.instagram.com">
+            <img className="snsIcon" src={instagram} alt="인스타그램" />
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
 
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,16 +3,14 @@ import "../assets/stylesheets/SearchBar.css";
 
 function SearchBar() {
   return (
-    <>
-      <div className="search-bar">
-        <input
-          id="search-input"
-          type="text"
-          placeholder="원하는 링크를 검색해 보세요"
-        ></input>
-        <img className="search-icon" src={searchIcon}></img>
-      </div>
-    </>
+    <div className="search-bar">
+      <input
+        id="search-input"
+        type="text"
+        placeholder="원하는 링크를 검색해 보세요"
+      ></input>
+      <img className="search-icon" src={searchIcon}></img>
+    </div>
   );
 }
 
