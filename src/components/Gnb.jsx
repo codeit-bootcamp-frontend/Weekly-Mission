1c6ce8a264fd2b4a0c4db315b09cf53c5110d836
redcoin96 <redcoin96@naver.com>
2023-05-01 18:32:20 +0900

    feat: components 추가

---
+++ src/components/Gnb.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import userData from "../api/userapi";
+import styles from "./Gnb.module.css";
+
+function Gnb() {
+  const [isLoggedIn, setisLoggedIn] = useState(true);
+  const [data, setdata] = useState(null);
+
+  useEffect(()=>{
+    async function fetchData(){
+      const data = await userData();
+      setdata(data)
+    }
+    fetchData()
+  },[])
+  
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <h1>
+            <Link to="/" className={styles.logo}></Link>
+          </h1>
+          {isLoggedIn ? 
+            <div className={styles.user}>
+              <img className={styles.userIcon} src={data && data.profileImageSource}/>
+              <span className={styles.userEmail}>{data && data.email}</span>
+            </div>
+           : 
+            <a href="/signin.html" className={styles.loginBtn}>
+              로그인
+            </a>
+          }
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Gnb;
---
+++ src/components/Star.jsx
@@ -0,0 +1,21 @@
+import childStyles from "./Star.module.css";
+
+function Star({ styles }) {
+
+  function toggleLikeIcon(e) {
+    e.preventDefault();
+    const star = e.target;
+    star.classList.toggle(childStyles.inactive);
+    star.classList.toggle(childStyles.active);
+  }
+
+  return (
+    <>
+      <div className = {styles.star}>
+        <button onClick={toggleLikeIcon} className={childStyles.inactive}></button>
+      </div>
+    </>
+  );
+}
+
+export default Star;
