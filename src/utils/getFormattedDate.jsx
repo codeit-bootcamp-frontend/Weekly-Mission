d73a8c66e1d05a0edb4016f8fabe8b56b7581c4c
Jer1s <flqmbl@gmail.com>
2023-05-01 14:35:22 +0900

    Refactor: Use join method at getFormattedDate function

--- src/utils/getFormattedDate.jsx
+++ src/utils/getFormattedDate.jsx
@@ -3,9 +3,7 @@ function getFormattedDate(dateItem) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  return `${year}. ${month < 10 ? "0" : ""}${month}. ${
-    day < 10 ? "0" : ""
-  }${day}.`;
+  return [year, month, day].join(". ");
 }
 
 export default getFormattedDate;
