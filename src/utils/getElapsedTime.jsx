8489b3ba96c8aa0d3c9bfc8123e657996743ffcc
Jer1s <flqmbl@gmail.com>
2023-05-04 00:09:17 +0900

    Refactor: Refactor getElapsedTime.jsx

--- src/utils/getElapsedTime.jsx
+++ src/utils/getElapsedTime.jsx
@@ -2,6 +2,9 @@ function getElapsedTime(dateItem) {
   if (dateItem === undefined) {
     return "";
   }
+  const createdDate = new Date(dateItem);
+  const nowDate = new Date();
+  const timeDiff = nowDate - createdDate;
 
   const MS_PER_MINUTE = 1000 * 60;
   const MS_PER_HOUR = MS_PER_MINUTE * 60;
@@ -9,51 +12,31 @@ function getElapsedTime(dateItem) {
   const MS_PER_MONTH = MS_PER_DAY * 30;
   const MS_PER_YEAR = MS_PER_DAY * 365;
 
-  const createdDate = new Date(dateItem);
-  const nowDate = new Date();
-  const timeDiff = nowDate - createdDate;
+  const timeUnits = [
+    { value: MS_PER_YEAR, label: "year" },
+    { value: MS_PER_MONTH, label: "month" },
+    { value: MS_PER_DAY, label: "day" },
+    { value: MS_PER_HOUR, label: "hour" },
+    { value: MS_PER_MINUTE, label: "minute" },
+  ];
 
-  let elapsedTime = "";
+  for (let i = 0; i < timeUnits.length; i++) {
+    const { value, label } = timeUnits[i];
 
-  switch (true) {
-    case timeDiff < 2 * MS_PER_MINUTE:
-      elapsedTime = `1 minute ago`;
-      break;
-    case timeDiff < MS_PER_HOUR:
-      const minuteDiff = Math.floor(timeDiff / MS_PER_MINUTE);
-      elapsedTime = `${minuteDiff} minutes ago`;
-      break;
-    case timeDiff < 2 * MS_PER_HOUR:
-      elapsedTime = `1 hour ago`;
-      break;
-    case timeDiff < MS_PER_DAY:
-      const hourDiff = Math.floor(timeDiff / MS_PER_HOUR);
-      elapsedTime = `${hourDiff} hours ago`;
-      break;
-    case timeDiff < 2 * MS_PER_DAY:
-      elapsedTime = `1 day ago`;
-      break;
-    case timeDiff < MS_PER_MONTH:
-      const dayDiff = Math.floor(timeDiff / MS_PER_DAY);
-      elapsedTime = `${dayDiff} days ago`;
-      break;
-    case timeDiff < 2 * MS_PER_MONTH:
-      elapsedTime = `1 month ago`;
-      break;
-    case timeDiff < MS_PER_YEAR:
-      const monthDiff = Math.floor(timeDiff / MS_PER_MONTH);
-      elapsedTime = `${monthDiff} months ago`;
-      break;
-    case timeDiff < 2 * MS_PER_YEAR:
-      elapsedTime = `1 year ago`;
-      break;
-    default:
-      const yearDiff = Math.floor(timeDiff / MS_PER_YEAR);
-      elapsedTime = `${yearDiff} years ago`;
-      break;
+    if (timeDiff < value) {
+      continue;
+    }
+
+    const formattedTimeDiff = Math.floor(timeDiff / value);
+
+    return (
+      formattedTimeDiff +
+      " " +
+      label +
+      (formattedTimeDiff > 1 ? "s" : "") +
+      " ago"
+    );
   }
-
-  return elapsedTime;
 }
 
 export default getElapsedTime;
