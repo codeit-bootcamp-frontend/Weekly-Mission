205be69025c5c62db18e1ba77f25015dda988cd5
Jer1s <flqmbl@gmail.com>
2023-05-08 22:37:21 +0900

    Feat: Add api.jsx

---
+++ src/utils/api.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://taskcomm-api-app.herokuapp.com/api";
+
+export const getUsers = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/users`);
+    return response.data;
+  } catch (error) {
+    console.error(`getUsers error: ${error}`);
+  }
+};
+
+export const getPosts = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/posts`);
+    return response.data;
+  } catch (error) {
+    console.error(`getFolders error: ${error}`);
+  }
+};
