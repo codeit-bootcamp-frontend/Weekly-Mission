d7aa2f30c099dc779a4987791c6d1b032f37f0d0
Jer1s <flqmbl@gmail.com>
2023-05-08 22:37:57 +0900

    Style: Edit misspelling

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -16,6 +16,6 @@ export const getPosts = async () => {
     const response = await axios.get(`${BASE_URL}/posts`);
     return response.data;
   } catch (error) {
-    console.error(`getFolders error: ${error}`);
+    console.error(`getPosts error: ${error}`);
   }
 };
