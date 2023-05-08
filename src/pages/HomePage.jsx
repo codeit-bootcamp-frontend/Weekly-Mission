b8e5cde260ef3caaffe7420a21030ab9c2488540
Jer1s <flqmbl@gmail.com>
2023-05-09 00:13:25 +0900

    Feat: Add logo to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,27 @@
+import styled from "styled-components";
+import LogoImg from "/logo.png";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  margin-top: 4rem;
+`;
+
+const Logo = styled.img`
+  width: 50rem;
+  object-fit: contain;
+`;
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <Container>
+      <Link to="/">
+        <Logo src={LogoImg} alt="Logo" />
+      </Link>
+    </Container>
+  );
 }
 
 export default HomePage;
