93e8b0d0d92db5daa676b51ea16e80b662ac5d05
sally <9609sally@gmail.com>
2023-05-22 21:00:48 +0900

    feat: Shared 페이지 컴포넌트 생성

---
+++ src/Components/CardList.jsx
@@ -0,0 +1,5 @@
+function CardList() {
+  return <div></div>;
+}
+
+export default CardList;
--- src/pages/shared/Shared.jsx
+++ src/pages/shared/Shared.jsx
@@ -1,11 +1,18 @@
 import Footer from "../../Components/Footer";
+import Gnb from "../../Components/Gnb";
+import SearchBar from "../../Components/SearchBar";
+import "./Shared.css";
 
 function Shared() {
   return (
     <>
+      <Gnb />
+      <SearchBar />
       <Footer />
     </>
   );
 }
 
 export default Shared;
+// useState에는 카드리스트만 넣고
+// 로그인은 어떻게?
