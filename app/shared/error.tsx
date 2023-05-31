f3b0c21053a22f4b3672c3326d61c6c302c313e7
wooleejaan <wooleejaan@gmail.com>
2023-05-31 15:09:47 +0900

    feat: shared 에러 처리 파일 생성

--- app/shared/error.tsx
+++ app/shared/error.tsx
@@ -9,10 +9,7 @@ export default function Error({
 }) {
   return (
     <div>
-      <h2>
-        Something went wrong! shared 페이지의 레이아웃 ~ 홈페이지에서 발생하는
-        에러들 처리
-      </h2>
+      <h2>Something went wrong! shared 페이지 내부 에러 담당</h2>
       <button onClick={() => reset()}>Try again</button>
     </div>
   );
