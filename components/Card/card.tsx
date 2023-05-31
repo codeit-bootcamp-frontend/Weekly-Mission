7b4c6895c1408b145046b8839d1729dcb9da58c7
wooleejaan <wooleejaan@gmail.com>
2023-05-31 21:38:49 +0900

    feat: card 컴포넌트 UI 작성

--- components/Card/card.tsx
+++ components/Card/card.tsx
@@ -2,13 +2,49 @@
 
 import { Link } from "@/components/CardWrapper/cardWrapper";
 import styles from "./card.module.css";
+import beautifyDate from "@/lib/beautifyData";
+import Image from "next/image";
 
 interface Props {
   link: Link;
 }
 
 const Card = ({ link }: Props) => {
-  return <div>Card</div>;
+  const { beautifiedDate, beautifiedTimeDiff } = beautifyDate(link.createdAt);
+
+  return (
+    <div className={styles.card} onClick={() => {}}>
+      <div className={styles.cardAsterisk}>
+        <Image
+          className={styles.image}
+          src="/assets/card-asterisk.svg"
+          alt="Card Asterisk"
+          fill
+        />
+      </div>
+      <div className={styles.cardImgTop}>
+        <Image
+          className={styles.image}
+          src={link.imageSource ?? "/assets/image-dummy.png"}
+          alt={link.title}
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1199px) 100vw, 100vw"
+        />
+      </div>
+      <div className={styles.cardCaption}>
+        <div className={styles.info}>
+          <span className={styles.time}>{beautifiedTimeDiff}</span>
+          <div className={styles.more}>
+            <span className={styles.moreDot}></span>
+            <span className={styles.moreDot}></span>
+            <span className={styles.moreDot}></span>
+          </div>
+        </div>
+        <p className={styles.text}>{link.description}</p>
+        <div className={styles.creation}>{beautifiedDate}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
