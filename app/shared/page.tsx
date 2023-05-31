07ceb895471eb50cfd7fc0a53a0cc42670af604d
wooleejaan <wooleejaan@gmail.com>
2023-05-31 20:13:06 +0900

    feat: /shared 페이지 내 히어로 콘텐츠 작성

--- app/shared/page.tsx
+++ app/shared/page.tsx
@@ -1,5 +1,26 @@
-const Shared = () => {
-  return <div>Shared</div>;
+import getFolderData from "@/lib/getFolderData";
+import styles from "./page.module.css";
+import Image from "next/image";
+
+const Shared = async () => {
+  const userFolder = await getFolderData();
+
+  return (
+    <main className={styles.main}>
+      <div className={`${styles.hero} ${styles.inner}`}>
+        <div className={styles.codeitAvatar}>
+          <Image
+            fill
+            src={userFolder.owner.profileImageSource}
+            alt="Owner Avatar"
+            className={styles.image}
+          />
+        </div>
+        <span className={styles.atsign}>@{userFolder.owner.name}</span>
+        <span className={styles.marks}>{userFolder.name}</span>
+      </div>
+    </main>
+  );
 };
 
 export default Shared;
