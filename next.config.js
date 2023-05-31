bd0fb1de9142f6fb88f6480c1daa40e3a9f1a5bf
wooleejaan <wooleejaan@gmail.com>
2023-05-31 21:36:34 +0900

    chore: next.config.js에 이미지 도메인 추가

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     domains: [
       "codeit-front.s3.ap-northeast-2.amazonaws.com",
       "codeit-images.codeit.com",
+      "codeit-frontend.codeit.com",
+      "reactjs.org",
+      "assets.vercel.com",
+      "github.com",
+      "storybook.js.org",
+      "testing-library.com",
+      "static.cdninstagram.com",
+      "s.pstatic.net",
     ],
   },
 };
