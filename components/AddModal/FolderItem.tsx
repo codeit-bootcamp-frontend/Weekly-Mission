d69c3528a3dad73e8a416de42fbfa681fb05a047
wooleejaan <wooleejaan@gmail.com>
2023-06-02 12:03:50 +0900

    feat: addModal 컴포넌트 구현

---
+++ components/AddModal/FolderItem.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Image from "next/image";
+
+import styles from "./FolderItem.module.css";
+
+const FolderItem = ({
+  folder,
+  idx,
+  isChecked,
+  setCheckedItemId,
+}: {
+  folder: { name: string; length: number };
+  idx: number;
+  isChecked: boolean;
+  setCheckedItemId: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  // TODO: API 받아오는 걸로 변경하면, folder prop 타입도 변경될 예정
+  return (
+    <div
+      className={`${styles.itemWrapper} ${styles[`${isChecked}`]}`}
+      onClick={() => setCheckedItemId(idx)}
+    >
+      {isChecked && (
+        <div className={styles.checkedIcon}>
+          <Image
+            className={styles.image}
+            fill
+            src="/assets/checked-folder.svg"
+            alt="checked folder"
+          />
+        </div>
+      )}
+      <div className={`${styles.linkName} ${styles[`${isChecked}`]}`}>
+        {folder.name}
+      </div>
+      <div className={`${styles.linkLength} ${styles[`${isChecked}`]}`}>
+        {folder.length}개 링크
+      </div>
+    </div>
+  );
+};
+
+export default FolderItem;
--- components/AddModal/addModal.tsx
+++ components/AddModal/addModal.tsx
@@ -1,18 +1,41 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+import Image from "next/image";
 
 import { allowScroll, preventScroll } from "@/lib/modal";
 
-import styles from "./addModal.module.css";
-import AddPortalWrapper from "./addPortalWrapper";
+import styles from "./AddModal.module.css";
+import AddPortalWrapper from "./AddPortalWrapper";
+import FolderItem from "./FolderItem";
 
 interface IAddModal {
-  handleClickCloseModal: () => void;
+  setOpenAddModal: React.Dispatch<React.SetStateAction<boolean>>;
   selectedLinkValue: string;
 }
 
-const AddModal = ({ handleClickCloseModal, selectedLinkValue }: IAddModal) => {
+const MOCK_FOLDERS: { name: string; length: number }[] = [
+  { name: "코딩팁", length: 7 },
+  { name: "채용 사이트", length: 12 },
+  { name: "유용한 글", length: 30 },
+  { name: "나만의 장소", length: 3 },
+];
+
+const AddModal = ({ setOpenAddModal, selectedLinkValue }: IAddModal) => {
+  // TODO: api로 폴더 데이터를 받아오면 MOCK 데이터 삭제 예정
+
+  const [checkedItemId, setCheckedItemId] = useState(-1);
+
+  const handleClickPostLink = (): void => {
+    // TODO: POST request : selectedLinkValue를 checkedItemId 를 가진 폴더에 저장
+    // TODO: 바로 닫히지 않고, 데이터가 잘 저장되었다고 알려주는 텀이 있으면 좋겠다.
+    const timer = setTimeout(() => {
+      setOpenAddModal(false);
+      clearTimeout(timer);
+    }, 500);
+  };
+
   useEffect(() => {
     const prevScrollY = preventScroll();
     return () => {
@@ -22,8 +45,41 @@ const AddModal = ({ handleClickCloseModal, selectedLinkValue }: IAddModal) => {
 
   return (
     <AddPortalWrapper>
-      <div className={styles.modalWrapper} onClick={handleClickCloseModal}>
-        <div></div>
+      <div className={styles.overlay} onClick={() => setOpenAddModal(false)}>
+        <div
+          className={styles.modalWrapper}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div
+            className={styles.closeWrapper}
+            onClick={() => setOpenAddModal(false)}
+          >
+            <Image
+              className={styles.image}
+              fill
+              src="/assets/modal-close.svg"
+              alt="close modal"
+            />
+          </div>
+          <div className={styles.contents}>
+            <h3 className={styles.title}>폴더에 추가</h3>
+            <h4 className={styles.subTitle}>링크 주소</h4>
+            <div className={styles.folderWrapper}>
+              {MOCK_FOLDERS.map((folder, idx) => (
+                <FolderItem
+                  key={folder.name}
+                  folder={folder}
+                  idx={idx}
+                  isChecked={idx === checkedItemId}
+                  setCheckedItemId={setCheckedItemId}
+                />
+              ))}
+            </div>
+            <button className={styles.addButton} onClick={handleClickPostLink}>
+              추가하기
+            </button>
+          </div>
+        </div>
       </div>
     </AddPortalWrapper>
   );
