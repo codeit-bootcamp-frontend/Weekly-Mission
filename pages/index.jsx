7934eb44ffefc0c9de1d55c083bb478d495019cf
Jer1s <flqmbl@gmail.com>
2023-06-03 21:40:48 +0900

    feat: 부트스트랩 흔적 제거

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Linkbrary</title>
+      </Head>
+      <div>Test</div>
+    </>
+  );
+};
+
+export default Home;
