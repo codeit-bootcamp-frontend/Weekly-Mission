2e793ef2d98bba92116ffac5e9aac1b6b26a78d9
Jer1s <flqmbl@gmail.com>
2023-06-03 22:24:40 +0900

    chore: tanstack query 셋업

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/forbid-prop-types */
+import { useState } from 'react';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import PropTypes from 'prop-types';
 
-/* eslint-disable react/jsx-props-no-spreading */
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => { return new QueryClient(); });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydrateState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 };
 
-/* eslint-disable react/forbid-prop-types */
 App.propTypes = {
   Component: PropTypes.func.isRequired,
   pageProps: PropTypes.object.isRequired,
