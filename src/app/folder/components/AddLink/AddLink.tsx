0e0d0d7893ea89df31ef491f5f4b5b0a503331ba
SeyoungCho <wnl383@naver.com>
2023-06-03 22:41:36 +0900

    feat: AddLink 컴포넌트 추가

---
+++ src/app/folder/components/AddLink/AddLink.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./AddLink.module.scss";
+
+const AddLink = () => {
+  return (
+    <form className={styles.addForm} action="/search/links?q=null">
+      <div>
+        <div>
+          <div className={styles.linkIconBox}>
+            <Image src="/link.svg" alt="link icon" fill />
+          </div>
+          <input
+            id="addlink-input"
+            type="text"
+            name="q"
+            placeholder="링크를 추가해 보세요"
+          />
+        </div>
+        <button type="submit" className={styles.addButton}>
+          추가하기
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AddLink;
---
+++ src/app/folder/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddLink from "./components/AddLink/AddLink";
+
+const Page = () => {
+  return (
+    <>
+      <AddLink />
+    </>
+  );
+};
+
+export default Page;
