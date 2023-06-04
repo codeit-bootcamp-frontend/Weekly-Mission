dda309e7c7bcbd57c15a1f9ad1a61b00b674af48
drizzle96 <dlxkffldk37@naver.com>
2023-06-05 01:07:21 +0900

    fix: 불필요 코드 삭제

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     "stylelint-config-property-sort-order-smacss",
   ],
   plugins: ["stylelint-scss"],
-  // ignoreFiles: ['src/styles/reset.scss', 'src/styles/common.scss'], // reset과 common scss는 ignore합니다.
   rules: {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
