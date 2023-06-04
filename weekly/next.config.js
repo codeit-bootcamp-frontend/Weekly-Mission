7a8d475bc2380d999f9d4d3cae2e35a03770d297
wooki <bwlimtony@gmail.com>
2023-06-05 02:27:10 +0900

    수정

--- weekly/next.config.js
+++ weekly/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    domains: [
+      "codeit-front.s3.ap-northeast-2.amazonaws.com",
+      "bootcamp-api.codeit.kr",
+      "codeit-frontend.codeit.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bootcamp-api.codeit.kr",
+        port: "",
+        pathname: "/api/sample/user",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
