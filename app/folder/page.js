123bee59a5f7bfe52a8246b254c97b844fba3135
hayoung-99 <nhy990130@gmail.com>
2023-06-06 01:29:56 +0900

    feat: folder 페이지용 AddLinkBar 컴포넌트 생성 & 적용

---
+++ app/folder/page.js
@@ -0,0 +1,21 @@
+import getUserData from "@/api/getUserData";
+import AddLinkBar from "@/components/AddLinkBar";
+import GNB from "@/components/GNB";
+import styles from "./page.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+export default async function Folder() {
+  const userData = await getUserData();
+  const { id: userId, name: userName, email, profileImageSource } = userData.data;
+
+  return (
+    <>
+      <GNB userEmail={email} userProfileImageSorce={profileImageSource} />
+      <header className={cx("header")}>
+        <AddLinkBar />
+      </header>
+    </>
+  );
+}
