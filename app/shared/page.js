dc89161807d78e0aa4197d9f46452de52a4ac412
hayoung-99 <nhy990130@gmail.com>
2023-06-05 21:08:25 +0900

    feat: shared page
    1. shared page에 넣은 폴더 유저 이미지 & GNB 로그인 유저 프로필 이미지를 외부에서 가져오기 때문에 next.config.js에서 도메인 추가

---
+++ app/shared/page.js
@@ -0,0 +1,42 @@
+import Card from "@/components/Card";
+import getFolderData from "@/api/getFolderData";
+import getUserData from "@/api/getUserData";
+import GNB from "@/components/GNB";
+import SearchBar from "@/components/SearchBar";
+import Footer from "@/components/Footer";
+import classNames from "classnames/bind";
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export default async function Shared() {
+  const folderData = await getFolderData();
+  const userData = await getUserData();
+
+  const { id: folderId, name: folderName, owner, links } = folderData.data.folder;
+  const { id: userId, name: userName, email, profileImageSource } = userData.data;
+
+  const cx = classNames.bind(styles);
+  console.log(email, profileImageSource);
+
+  return (
+    <>
+      <GNB userEmail={email} userProfileImageSorce={profileImageSource} />
+      <header className={cx("header")}>
+        <div className={cx("user-content")}>
+          <Image className={cx("user-avatar")} src={owner.profileImageSource} alt="avatar" width={60} height={60} />
+          <p className={cx("user-nickname")}>@{owner.name}</p>
+        </div>
+        <h1 className={cx("header-title")}>{folderName}</h1>
+      </header>
+      <main className={cx("main")}>
+        <SearchBar placeholder="원하는 링크를 검색해 보세요" />
+        <div className={cx("card-list")}>
+          {links.map((linkData) => (
+            <Card key={linkData.id} data={linkData} />
+          ))}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "codeit-frontend.codeit.com",
+      "reactjs.org",
+      "assets.vercel.com",
+      "github.com",
+      "storybook.js.org",
+      "testing-library.com",
+      "static.cdninstagram.com",
+      "s.pstatic.net",
+      "codeit-images.codeit.com", // folder user image
+      "codeit-front.s3.ap-northeast-2.amazonaws.com", // login user image
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
