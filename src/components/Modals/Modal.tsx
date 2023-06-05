dc59534f4279dcecba839f8933b42e3998bab033
SeyoungCho <wnl383@naver.com>
2023-06-05 11:32:47 +0900

    feat: Modal컴포넌트 제작

---
+++ src/components/Modals/Modal.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./Modal.module.scss";
+import Image from "next/image";
+
+interface ModalProps {
+  type: string;
+  modalRef: React.RefObject<HTMLDialogElement>;
+  title: string;
+  subtitle?: string;
+  ui?: React.ReactNode;
+  proceedBtnText?: string;
+  onClickProceed?: (() => {}) | (() => void);
+  onClose: () => void;
+}
+
+const Modal = ({
+  type,
+  modalRef,
+  title,
+  subtitle,
+  ui,
+  proceedBtnText,
+  onClickProceed,
+  onClose,
+}: ModalProps) => {
+  return (
+    <dialog className={styles.modalContainer} id="modal" ref={modalRef}>
+      <div className={styles.closeIconBox} onClick={onClose}>
+        <Image src="/close-modal-icon.svg" alt="close modal" fill />
+      </div>
+      <div className={styles.titleContainer}>
+        <h1>{title}</h1>
+        {subtitle && <h3>{subtitle}</h3>}
+      </div>
+      <div className={styles.uiContainer}>{ui ?? null}</div>
+      {proceedBtnText && (
+        <button
+          className={`${styles.proceedBtn} ${styles[`${type}`]}`}
+          type="button"
+          onClick={onClickProceed}
+        >
+          {proceedBtnText}
+        </button>
+      )}
+    </dialog>
+  );
+};
+
+export default Modal;
