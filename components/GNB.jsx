0bee95c65e51fbf19ad0e2ff9629e76875e7c773
hayoung-99 <nhy990130@gmail.com>
2023-06-05 15:16:54 +0900

    refactor: GNB - classNames 패키지 적용

--- components/GNB.jsx
+++ components/GNB.jsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/GNB.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 export default function GNB() {
   return (
-    <nav className={styles["gnb"]}>
-      <Link className={styles["linkbrary-logo"]} href="/">
-        <Image fill className={styles["linkbrary-logo-image"]} src="/images/linkbrary.svg" alt="linkbrary 로고 이미지" />
+    <nav className={cx("gnb")}>
+      <Link className={cx("linkbrary-logo")} href="/">
+        <Image fill className={cx("linkbrary-logo-image")} src="/images/linkbrary.svg" alt="linkbrary 로고 이미지" />
       </Link>
-      <Link className={styles["login-button"]} href="/">
+      <Link className={cx("login-button")} href="/">
         로그인
       </Link>
     </nav>
