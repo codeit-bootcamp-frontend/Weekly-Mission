fc572db0b5071b58a5aa756d006591dfae711766
Jer1s <flqmbl@gmail.com>
2023-06-06 13:25:29 +0900

    feat: modal 닫는 이벤트 추가

--- components/FolderModal.jsx
+++ components/FolderModal.jsx
@@ -1,12 +1,43 @@
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Image from 'next/image';
 import styles from './FolderModal.module.css';
+import closeIcon from '@/public/close.svg';
 
 const FolderModal = ({ option, closeModal }) => {
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (e.target.classList.contains(styles.modalOverlay)) {
+        closeModal();
+      }
+    };
+
+    const handleEscKey = (e) => {
+      if (e.keyCode === 27) {
+        closeModal();
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+    document.addEventListener('keydown', handleEscKey);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+      document.removeEventListener('keydown', handleEscKey);
+    };
+  }, [closeModal]);
+
   return (
     <div className={styles.modalOverlay}>
       <div className={styles.modal}>
         {option}
-        <button type="button" onClick={closeModal}>x</button>
+        <button className={styles.closeButton} type="button" onClick={closeModal}>
+          <Image
+            fill
+            src={closeIcon}
+            alt="Close"
+          />
+        </button>
       </div>
     </div>
   );
