0f4e54ba67d392ab83e90de0e184e14091390ffe
Yejin Shin <shinyejin0212@gmail.com>
2023-06-07 18:28:29 +0900

    feat: 조건에 따른 folder 렌더링 구현

--- src/pages/folder/[[...slug]].js
+++ src/pages/folder/[[...slug]].js
@@ -3,31 +3,35 @@ import { useRouter } from "next/router";
 import styles from "@/styles/folder.module.css";
 import SearchBar from "@/components/SearchBar";
 import CardList from "@/components/CardList";
-import PageInfo from "@/components/PageInfo";
 import DefaultLayout from "@/layouts/DefaultLayout";
-import EditModal from "@/components/Modals/EditModal";
-import AddFolderModal from "@/components/Modals/AddFolderModal";
-import DeleteFolderModal from "@/components/Modals/DeleteFolderModal";
 import AddLinkBar from "@/components/AddLinkBar";
 import FolderMenu from "@/components/FolderMenu/FolderMenu";
+import FolderHeader from "@/components/FolderHeader/FolderHeader";
+
 const BASE_URL = process.env.BASE_URL;
 
-const FolderPage = ({ folder, links }) => {
+const MOCK_FOLDERS = [
+  { id: 0, folderTitle: "전체" },
+  { id: 1, folderTitle: "⭐️ 즐겨찾기" },
+  { id: 2, folderTitle: "코딩 팁" },
+  { id: 3, folderTitle: "채용 사이트" },
+  { id: 4, folderTitle: "유용한 글" },
+  { id: 5, folderTitle: "나만의 장소" },
+];
+
+const FolderPage = ({ links }) => {
   const router = useRouter();
   const { slug } = router.query;
 
   let currentTab;
-
-  switch (slug) {
-    case undefined:
-      currentTab = 0;
-      break;
-    case "favorites":
-      currentTab = 1;
-      break;
-    default:
-      currentTab = Number(slug);
-      break;
+  if (!slug) {
+    currentTab = MOCK_FOLDERS[0].folderTitle;
+  } else if (slug.length === 1) {
+    if (slug[0] === "favorites") {
+      currentTab = MOCK_FOLDERS[1].folderTitle;
+    } else {
+      currentTab = MOCK_FOLDERS[Number(slug[0]) + 1].folderTitle;
+    }
   }
 
   return (
@@ -37,24 +41,37 @@ const FolderPage = ({ folder, links }) => {
       </div>
       <div className={styles.wrapper}>
         <SearchBar />
+        <FolderMenu currentTab={currentTab} MOCK_FOLDERS={MOCK_FOLDERS} />
+        <FolderHeader currentTab={currentTab} />
       </div>
       <div className={styles.cardWrapper}>
-        <FolderMenu currentTab={currentTab} />
-        <CardList cards={links} />
+        {links.length !== 0 ? (
+          <CardList cards={links} />
+        ) : (
+          <div className={styles.emptySavedLink}>저장한 링크가 없습니다</div>
+        )}
       </div>
-      {/* <AddFolderModal /> */}
     </>
   );
 };
 
-export async function getServerSideProps() {
+export async function getServerSideProps({ query }) {
+  const { slug } = query;
+
+  if (slug) {
+    return {
+      props: {
+        links: [],
+      },
+    };
+  }
+
   const response = await fetch(`${BASE_URL}api/sample/folder`);
   const data = await response.json();
-  const { folder = {}, folder: { links = [] } = {} } = data?.data ?? {};
+  const { folder: { links = [] } = {} } = data?.data ?? {};
 
   return {
     props: {
-      folder,
       links,
     },
   };
