03a4654ab132ae7d99c00575d7ca1079160f80e5
wooleejaan <wooleejaan@gmail.com>
2023-06-09 03:12:26 +0900

    style: 컴포넌트에 module.css => scss 적용

--- components/Gnb/Gnb.tsx
+++ components/Gnb/Gnb.tsx
@@ -12,33 +12,35 @@ export interface IGnb {
 
 const Gnb = ({ user }: IGnb) => {
   return (
-    <header className={`${styles.header}`}>
-      <div className={`${styles.inner}`}>
-        <nav className={`${styles.nav}`}>
-          <Link href="/" className={`${styles.logo}`}>
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <nav className={styles.nav}>
+          <Link href="/" className={styles.logo}>
             <Image
               src="/assets/linkbrary-logo.svg"
               alt="Linkbrary Logo"
               width="133"
               height="25"
               priority
+              className={styles.image}
             />
           </Link>
           {Object.keys(user).length === 0 ? (
-            <Link href="/signin" className={`${styles.signin}`}>
+            <Link href="/signin" className={styles.signin}>
               로그인
             </Link>
           ) : (
-            <Link href="" className={`${styles.user}`}>
-              <div className={`${styles.imgContainer}`}>
+            <Link href="" className={styles.user}>
+              <div className={styles.imgContainer}>
                 <Image
                   src={user.profileImageSource}
                   alt={user.name}
                   width="28"
                   height="28"
+                  className={styles.image}
                 />
               </div>
-              <span>{user.email}</span>
+              <span className={styles.email}>{user.email}</span>
             </Link>
           )}
         </nav>
