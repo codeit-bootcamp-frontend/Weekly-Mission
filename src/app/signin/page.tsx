5b62519670bb5c9b62f44e82c242c98bd04260dd
SeyoungCho <wnl383@naver.com>
2023-06-09 16:18:56 +0900

    feat: 로그인 페이지 반응형 디자인 적용

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,55 +1,60 @@
-import React from "react";
+"use client";
+
 import styles from "./page.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <article className={styles.loginForm}>
-      <section className={styles.logoContainer}>
-        <a href="/index.html">
-          <img src="images/logo.svg" alt="logo" />
-        </a>
-      </section>
-      <section className={styles.createAccountText}>
-        <p>
-          회원이 아니신가요?<a href="/signup.html">회원 가입하기</a>
-        </p>
-      </section>
-      <form className={styles.signinForm}>
-        <label htmlFor="username">이메일</label>
-        <br />
-        <input id="username" type="email" name="username" />
-        <label htmlFor="password">비밀번호</label>
-        <br />
-        <div className={styles.passwordField}>
-          <input id="password" type="password" name="password" />
-          <div id="show-password-icon" className={styles.showPasswordIcon}>
-            <img src="/images/show-password-icon.svg" alt="show password" />
+    <main className={styles.container}>
+      <article className={styles.loginForm}>
+        <section className={styles.logoContainer}>
+          <Link href="/">
+            <Image src="/logo.svg" alt="logo" fill />
+          </Link>
+        </section>
+        <section className={styles.createAccountText}>
+          <p>
+            회원이 아니신가요?<a href="/signup">회원 가입하기</a>
+          </p>
+        </section>
+        <form className={styles.signinForm}>
+          <label htmlFor="username">이메일</label>
+          <br />
+          <input id="username" type="email" name="username" />
+          <label htmlFor="password">비밀번호</label>
+          <br />
+          <div className={styles.passwordField}>
+            <input id="password" type="password" name="password" />
+            <div id="show-password-icon" className={styles.showPasswordIcon}>
+              <Image src="/show-password-icon.svg" alt="show password" fill />
+            </div>
           </div>
-        </div>
-        <input
-          id="login-btn"
-          className={styles.loginBtn}
-          type="submit"
-          value="로그인"
-        />
-      </form>
-      <section className={styles.findPassword}>
-        <a href="/forgot-password.html">비밀번호 찾기</a>
-      </section>
-      <section className={styles.socialLoginContainer}>
-        <p>소셜 로그인</p>
-        <div className={styles.socialLogoBox}>
-          <a href="https://www.google.com/">
-            <img src="/images/google-icon.png" alt="google logo" />
-          </a>
-        </div>
-        <div className="social-logo-box">
-          <a href="https://www.kakaocorp.com/page/">
-            <img src="/images/kakao-icon.png" alt="kakao logo" />
-          </a>
-        </div>
-      </section>
-    </article>
+          <input
+            id="login-btn"
+            className={styles.loginBtn}
+            type="submit"
+            value="로그인"
+          />
+        </form>
+        <section className={styles.findPassword}>
+          <a href="/forgot-password">비밀번호 찾기</a>
+        </section>
+        <section className={styles.socialLoginContainer}>
+          <p>소셜 로그인</p>
+          <div className={styles.socialLogoBox}>
+            <Link href="https://www.google.com/">
+              <Image src="/google-icon.png" alt="google logo" fill />
+            </Link>
+          </div>
+          <div className={styles.socialLogoBox}>
+            <Link href="https://www.kakaocorp.com/page/">
+              <Image src="/kakao-icon.png" alt="kakao logo" fill />
+            </Link>
+          </div>
+        </section>
+      </article>
+    </main>
   );
 };
 
