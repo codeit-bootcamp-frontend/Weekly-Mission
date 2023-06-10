21e0072bfe9bd89d1743ea2346829d0280bf62ad
wooleejaan <wooleejaan@gmail.com>
2023-06-10 16:15:27 +0900

    feat: /folder 페이지에도 react-query 도입

--- app/folder/layout.tsx
+++ app/folder/layout.tsx
@@ -1,24 +1,34 @@
 import Footer from "@/components/Footer/Footer";
 import Gnb from "@/components/Gnb/Gnb";
-import getUserData from "@/lib/getUserData";
+import QueryHydrate from "@/components/QueryHydrate/QueryHydrate";
+import getQueryClient from "@/lib/tanstack/getQueryClient";
+import { getUserQueryFn } from "@/lib/tanstack/queryFns/foldersQueryFns";
+import { dehydrate } from "@tanstack/react-query";
+
+export const revalidate = 3600;
 
 export default async function FolderLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const user = await getUserData();
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(["user"], getUserQueryFn);
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <>
-      <Gnb user={user} />
-      {children}
-      <Footer />
-      <div id="add-portal"></div>
-      <div id="delete-link-portal"></div>
-      <div id="add-folder-portal"></div>
-      <div id="share-folder-portal"></div>
-      <div id="edit-folder-portal"></div>
-      <div id="delete-folder-portal"></div>
+      <QueryHydrate state={dehydratedState}>
+        <Gnb />
+        {children}
+        <Footer />
+        <div id="add-portal"></div>
+        <div id="delete-link-portal"></div>
+        <div id="add-folder-portal"></div>
+        <div id="share-folder-portal"></div>
+        <div id="edit-folder-portal"></div>
+        <div id="delete-folder-portal"></div>
+      </QueryHydrate>
     </>
   );
 }
