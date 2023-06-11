38b00106569314044f835642e738aa8c06c91a04
Yejin Shin <shinyejin0212@gmail.com>
2023-06-12 01:35:35 +0900

    feat: DeleteFolderModal tsx로 migration

--- src/components/Modals/DeleteFolderModal.tsx
+++ src/components/Modals/DeleteFolderModal.tsx
@@ -1,15 +1,23 @@
+import React from "react";
 import ModalContainer from "@/components/Modals/ModalContainer";
-import InputBox from "@/presentation/Input";
 import SubmitButton from "@/presentation/Button/SubmitButton";
+
+interface DeleteFolderModalProps {
+  isFolderDeleteModalOpen: boolean;
+  onClose: () => void;
+  currentFolderTitle: string;
+}
+
 const DeleteFolderModal = ({
   isFolderDeleteModalOpen,
   onClose,
   currentFolderTitle,
-}) => {
-  const handleSubmit = (e) => {
+}: DeleteFolderModalProps) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onClose();
   };
+
   return (
     <ModalContainer
       modalTitle="폴더 삭제"
@@ -18,7 +26,7 @@ const DeleteFolderModal = ({
       onClose={onClose}
     >
       <form onSubmit={handleSubmit}>
-        <SubmitButton buttonType="red" buttonText="삭제하기" type="submit" />
+        <SubmitButton buttonType="red" buttonText="삭제하기" />
       </form>
     </ModalContainer>
   );
