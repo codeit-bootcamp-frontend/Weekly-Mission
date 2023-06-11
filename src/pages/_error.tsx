17c2a0bdf77015d063fed4da1f44d13d730efcec
Yejin Shin <shinyejin0212@gmail.com>
2023-06-11 23:23:54 +0900

    feat: _document & _error tsx로 migration

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,14 +1,17 @@
 import DefaultLayout from "@/layouts/DefaultLayout";
-import React from "react";
 
-export default function Error() {
+const Error: React.FC & {
+  getLayout: (page: JSX.Element) => JSX.Element;
+} = () => {
   return (
     <div>
       <p>에러가 발생했습니다</p>
     </div>
   );
-}
+};
 
 Error.getLayout = function getLayout(page) {
   return <DefaultLayout>{page}</DefaultLayout>;
 };
+
+export default Error;
