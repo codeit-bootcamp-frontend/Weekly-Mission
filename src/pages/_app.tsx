0acfe3bb4ac2935347b4c593c7a56189a642cdc6
Yejin Shin <shinyejin0212@gmail.com>
2023-06-11 23:15:33 +0900

    feat: _app.tsx로 migration

---
+++ src/pages/_app.tsx
@@ -0,0 +1,51 @@
+import { UserProvider } from "@/contexts/UserProvider";
+import getData from "@/lib/getData";
+import "@/styles/global.css";
+import type { AppProps } from "next/app";
+import type { NextPage, NextPageContext } from "next";
+import { ReactElement, ReactNode } from "react";
+import { User } from "$/types";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+interface MyAppProps extends AppPropsWithLayout {
+  userInfo: User;
+}
+
+function MyApp({ Component, pageProps, userInfo }: MyAppProps): ReactElement {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <UserProvider initialUser={userInfo}>
+      {getLayout(<Component {...pageProps} />)}
+    </UserProvider>
+  );
+}
+
+MyApp.getInitialProps = async ({
+  Component,
+  ctx,
+}: MyAppProps & { ctx: NextPageContext }) => {
+  // TODO: 추후 인증으로 변경해야함.
+  const userId = 1;
+  const data = await getData(`/api/users/${userId}`);
+  const userInfo = data.data[0];
+
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return {
+    pageProps,
+    userInfo: userInfo || "",
+  };
+};
+
+export default MyApp;
