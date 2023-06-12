6fef7c1ed4bc6f6cb732ce903aea9667387562a1
wooleejaan <wooleejaan@gmail.com>
2023-06-12 22:10:29 +0900

    refactor: LinkCard 컴포넌트 안에서 케밥 UI 로직 분리

--- components/LinkCard/LinkCard.tsx
+++ components/LinkCard/LinkCard.tsx
@@ -4,12 +4,10 @@ import { useRef, useState } from "react";
 
 import Image from "next/image";
 
-import AddLinkModal from "@/components/Modals/AddLinkModal/AddLinkModal";
-import DeleteLinkModal from "@/components/Modals/DeleteLinkModal/DeleteLinkModal";
-import useOutsideClick from "@/hooks/useOutsideClick";
 import { ILink } from "@/types/linkbrary";
 import beautifyDate from "@/utils/beautifyDate";
 
+import Kebab from "./Kebab";
 import styles from "./LinkCard.module.scss";
 
 interface ILinkCardProps {
@@ -28,11 +26,6 @@ const LinkCard = ({
   const { beautifiedDate, beautifiedTimeDiff } = beautifyDate(link.created_at);
   const [isCheckAsterisk, setIsCheckAsterisk] = useState<boolean>(false);
   const notTargetRefs = useRef<HTMLElement[]>([]);
-  const kebabRef = useRef<HTMLDivElement | null>(null);
-
-  const [openDeleteLinkModal, setOpenDeleteLinkModal] =
-    useState<boolean>(false);
-  const [openAddLinkModal, setOpenAddLinkModal] = useState<boolean>(false);
 
   const handleClickNavigation = (e: React.MouseEvent<HTMLDivElement>): void => {
     if (isClickedKebab) return;
@@ -42,8 +35,6 @@ const LinkCard = ({
 
     if (isValidTarget) window.open(link.url);
   };
-
-  useOutsideClick(kebabRef, handleClickCloseKebab);
 
   return (
     <div className={styles.cardWrapper}>
@@ -86,43 +77,13 @@ const LinkCard = ({
         </div>
       </div>
 
-      <div
+      <Kebab
         ref={(el: HTMLDivElement) => (notTargetRefs.current[1] = el)}
-        className={styles.kebabMenu}
-        onClick={handleClickOpenKebab}
-      >
-        <span className={styles.kebabDot}></span>
-        <span className={styles.kebabDot}></span>
-        <span className={styles.kebabDot}></span>
-        {isClickedKebab && (
-          <div className={styles.popOverWrapper} ref={kebabRef}>
-            <div
-              className={styles.deleteButton}
-              onClick={() => setOpenDeleteLinkModal(true)}
-            >
-              삭제하기
-            </div>
-            <div
-              className={styles.addFolderButton}
-              onClick={() => setOpenAddLinkModal(true)}
-            >
-              폴더에 추가
-            </div>
-          </div>
-        )}
-      </div>
-      {openAddLinkModal && (
-        <AddLinkModal
-          setOpenAddLinkModal={setOpenAddLinkModal}
-          selectedLinkValue={link.url}
-        />
-      )}
-      {openDeleteLinkModal && (
-        <DeleteLinkModal
-          setOpenDeleteLinkModal={setOpenDeleteLinkModal}
-          selectedLinkValue={link.url}
-        />
-      )}
+        linkUrl={link.url}
+        isClickedKebab={isClickedKebab}
+        handleClickOpenKebab={handleClickOpenKebab}
+        handleClickCloseKebab={handleClickCloseKebab}
+      />
     </div>
   );
 };
