1daa7ce8a12f3107f30b789445535a4268e30e5a
hayoung-99 <nhy990130@gmail.com>
2023-06-14 11:01:33 +0900

    feat: 링크를 여러 개 폴더에 추가하는 UX 설정

--- components/ModalAdd.jsx
+++ components/ModalAdd.jsx
@@ -1,16 +1,39 @@
+"use client";
+
+import { useState } from "react";
 import classNames from "classnames/bind";
 import Modal from "./Modal";
 import styles from "@/styles/ModalAdd.module.css";
 import SelectFolders from "./SelectFolders";
 
+const MOCK_FOLDERS = [
+  { name: "코딩팁", n_of_links: 7, selected: false },
+  { name: "채용 사이트", n_of_links: 12, selected: true },
+  { name: "유용한 글", n_of_links: 30, selected: false },
+  { name: "나만의 장소", n_of_links: 3, selected: false },
+];
+
 const cx = classNames.bind(styles);
 
 export default function ModalAdd({ link = "https://www.abc.com", onClose }) {
+  const [folders, setFolders] = useState(MOCK_FOLDERS);
+
+  const handleChange = (folderNum) => {
+    folders[folderNum]["selected"] = !folders[folderNum]["selected"];
+    setFolders([...folders]);
+    console.log(folders);
+  };
+
+  const handleSubmit = () => {
+    console.log("폴더 추가", folders);
+    onClose(false);
+  };
+
   return (
     <Modal title="폴더에 추가" onClose={onClose}>
       <p className={cx("link-title")}>{link}</p>
-      <SelectFolders />
-      <button className={cx("add-button")} type="button" onClick={() => console.log("폴더 추가")}>
+      <SelectFolders folders={folders} onClick={handleChange} />
+      <button className={cx("add-button")} type="button" onClick={() => handleSubmit()}>
         추가하기
       </button>
     </Modal>
--- components/SelectFolders.jsx
+++ components/SelectFolders.jsx
@@ -1,22 +1,23 @@
 "use client";
 
+import { useState } from "react";
 import styles from "@/styles/SelectFolder.module.css";
 import classNames from "classnames/bind";
 import Image from "next/image";
 
-const MOCK_FOLDER = [
-  { name: "코딩팁", n_of_links: 7, selected: false },
-  { name: "채용 사이트", n_of_links: 12, selected: true },
-  { name: "유용한 글", n_of_links: 30, selected: false },
-  { name: "나만의 장소", n_of_links: 3, selected: false },
-];
+// const MOCK_FOLDER = [
+//   { name: "코딩팁", n_of_links: 7, selected: false },
+//   { name: "채용 사이트", n_of_links: 12, selected: true },
+//   { name: "유용한 글", n_of_links: 30, selected: false },
+//   { name: "나만의 장소", n_of_links: 3, selected: false },
+// ];
 
 const cx = classNames.bind(styles);
 
-function FolderList({ folderData }) {
+function FolderItem({ folderData, folderNumber, onClick }) {
   return (
     <li className={cx("folder-list-item", { selected: folderData.selected })}>
-      <button type="button" className={cx("folder-list-item-button")} onClick={() => console.log("폴더 클릭")}>
+      <button type="button" className={cx("folder-list-item-button")} onClick={() => onClick(folderNumber)}>
         <p className={cx("folder-title")}>{folderData.name}</p>
         <p className={cx("number-of-folder")}>{folderData["n_of_links"]}개 링크</p>
         {folderData.selected && <Image className={cx("check")} src="/images/check.svg" alt="선택" width={14} height={14} />}
@@ -25,11 +26,18 @@ function FolderList({ folderData }) {
   );
 }
 
-export default function SelectFolders({ folders = MOCK_FOLDER }) {
+export default function SelectFolders({ folders, onClick }) {
+  // const [folders, setFolders] = useState(foldersData);
+  // const handleChange = (folderNum) => {
+  //   folders[folderNum]["selected"] = !folders[folderNum]["selected"];
+  //   setFolders([...folders]);
+  //   console.log(folders);
+  // };
+
   return (
     <ol className={cx("folder-list")}>
-      {folders.map((folder) => (
-        <FolderList key={folder.name} folderData={folder} />
+      {folders.map((folder, i) => (
+        <FolderItem key={folder.name} folderNumber={i} folderData={folder} onClick={onClick} />
       ))}
     </ol>
   );
