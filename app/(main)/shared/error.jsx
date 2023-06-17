31ec41e52829dea2bdc9a884b3125e7afeffee35
drizzle96 <dlxkffldk37@naver.com>
2023-06-17 17:17:52 +0900

    feat: 에러 처리(404 not-found 포함)

--- app/(main)/shared/error.jsx
+++ app/(main)/shared/error.jsx
@@ -5,15 +5,13 @@ import PropTypes from "prop-types";
 
 export default function GlobalError({ error, reset }) {
   return (
-    <html>
-      <body>
-        <h2>Error: {error.message}</h2>
-        <button onClick={() => reset()}>Try again</button>
-        <div>
-          <Link href="/">홈으로 돌아가기</Link>
-        </div>
-      </body>
-    </html>
+    <>
+      <h2>Error: {error.message}</h2>
+      <button onClick={() => reset()}>Try again</button>
+      <div>
+        <Link href="/">홈으로 돌아가기</Link>
+      </div>
+    </>
   );
 }
 
