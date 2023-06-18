b9a5af648c4ae1c0429bd16986214348b26c7438
drizzle96 <dlxkffldk37@naver.com>
2023-06-18 11:55:27 +0900

    feat: 실행 환경에 따라 API baseURL를 다르게 적용

--- utils/api/index.js
+++ utils/api/index.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "https://bootcamp-api.codeit.kr/api",
   timeout: 5000,
   headers: {},
 });
