ffc7904f6c620f47d00b8759aaf96a171d9b9972
drizzle96 <dlxkffldk37@naver.com>
2023-06-18 17:48:03 +0900

    feat: 반복 사용 타입 types.ts 모듈로 생성

---
+++ types.ts
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+
+export interface User {
+  id: number;
+  created_at: string;
+  name: string;
+  image_source: string;
+  email: string;
+}
+
+export interface Folder {
+  id: number;
+  created_at: string;
+  name: string;
+  user_id: number;
+}
+
+export interface Link {
+  id: number;
+  created_at: string;
+  updated_at: string | null;
+  url: string;
+  title: string;
+  description: string;
+  image_source: string | null;
+}
+
+export interface Children {
+  children: ReactNode;
+}
+
+export interface ErrorProps {
+  error: Error;
+  reset: () => void;
+}
