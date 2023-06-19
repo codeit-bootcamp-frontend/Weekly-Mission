12b7de8a307c99a38df576fcd17fb837d2b9537f
drizzle96 <dlxkffldk37@naver.com>
2023-06-19 11:46:05 +0900

    feat: faq, policy 사이트 생성

---
+++ app/(main)/faq/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Linkbrary : FAQ",
+};
+
+export default function FAQ() {
+  return <h1>faq 페이지입니다.</h1>;
+}
---
+++ app/(main)/policy/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Linkbrary : Policy",
+};
+
+export default function Policy() {
+  return <h1>policy 페이지입니다.</h1>;
+}
