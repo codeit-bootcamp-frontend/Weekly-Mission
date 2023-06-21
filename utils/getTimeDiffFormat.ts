24c8432aefdb5c3b28161d319fbe41a6e35c2d96
hayoung-99 <nhy990130@gmail.com>
2023-06-21 21:48:31 +0900

    test: data

--- utils/getTimeDiffFormat.ts
+++ utils/getTimeDiffFormat.ts
@@ -1,5 +1,5 @@
 export default function getTimeDiffFormat(dateString: string) {
-  const now = new Date();
+  const now = new Date("2023-06-21");
   const createdDate = new Date(dateString);
 
   const diffMSec = now.getTime() - createdDate.getTime();
