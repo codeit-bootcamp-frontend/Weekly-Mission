bbdddc6943d5b7369645ecca3d17abf19af15033
hayoung-99 <nhy990130@gmail.com>
2023-06-21 13:46:16 +0900

    feat: mock, default 관련 데이터 파일 분리

---
+++ data/mock.ts
@@ -0,0 +1,6 @@
+export const MOCK_FOLDERS = [
+  { name: "코딩팁", n_of_links: 7, selected: false },
+  { name: "채용 사이트", n_of_links: 12, selected: true },
+  { name: "유용한 글", n_of_links: 30, selected: false },
+  { name: "나만의 장소", n_of_links: 3, selected: false },
+];
