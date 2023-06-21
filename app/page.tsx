9371436d9a4ac1b4a8e59c6b80fda9c5c43bea79
wooleejaan <wooleejaan@gmail.com>
2023-06-21 16:11:32 +0900

    fix: 기존 로그인 로직을 위해 작성했던 리액트 쿼리 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,153 +1,134 @@
 import Image from "next/image";
-import { redirect } from "next/navigation";
 
 import Footer from "@/components/Footer/Footer";
 import Gnb from "@/components/Gnb/Gnb";
-import QueryHydrate from "@/components/QueryHydrate/QueryHydrate";
-import getQueryClient from "@/lib/tanstack/getQueryClient";
-import { getUserQueryFn } from "@/lib/tanstack/queryFns/foldersQueryFns";
-import { dehydrate } from "@tanstack/react-query";
-import { getServerSession } from "next-auth";
+import getCurrentUser from "@/lib/getCurrentUser";
 
-import { authOptions } from "./api/auth/[...nextauth]/route";
 import styles from "./page.module.scss";
 
 export const revalidate = 3600;
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
-  const userId = session.user.id;
-
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(["user"], () =>
-    getUserQueryFn(userId as number)
-  );
-  const dehydratedState = dehydrate(queryClient);
+  const userProfile = await getCurrentUser();
 
   return (
     <>
-      <QueryHydrate state={dehydratedState}>
-        <Gnb userId={userId as number} />
-        <main className={styles.main}>
-          <div className={styles.hero}>
-            <div className={styles.inner}>
-              <div className={styles.title}>
-                <span>세상의 모든 정보</span>를<br />
-                <div>
-                  <p>쉽게 저장하고</p>
-                  <p>관리해 보세요</p>
-                </div>
+      <Gnb user={userProfile} />
+      <main className={styles.main}>
+        <div className={styles.hero}>
+          <div className={styles.inner}>
+            <div className={styles.title}>
+              <span>세상의 모든 정보</span>를<br />
+              <div>
+                <p>쉽게 저장하고</p>
+                <p>관리해 보세요</p>
               </div>
-              <div className={styles.add}>링크 추가하기</div>
-              <div className={styles.imgContainer}>
+            </div>
+            <div className={styles.add}>링크 추가하기</div>
+            <div className={styles.imgContainer}>
+              <Image
+                src="/assets/img-info.png"
+                alt="Hero Image"
+                width={1168}
+                height={561}
+                priority
+                className={styles.img}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.contents}>
+          <div className={styles.inner}>
+            <section className={`${styles.save} ${styles.section}`}>
+              <h2>
+                <span>
+                  <span className={styles.gradient}>원하는 링크</span>를
+                </span>
+                <span>저장하세요</span>
+              </h2>
+              <p>
+                나중에 읽고 싶은 글, 다시 보고 싶은 영상, 사고 싶은 옷, 기억하고
+                싶은 모든 것을 한 공간에 저장하세요.
+              </p>
+              <div className={styles.imageContainer}>
                 <Image
-                  src="/assets/img-info.png"
-                  alt="Hero Image"
-                  width={1168}
-                  height={561}
-                  priority
-                  className={styles.img}
+                  src="/assets/save-link.png"
+                  alt="Save Link"
+                  width={1100}
+                  height={900}
+                  className={styles.image}
                 />
               </div>
-            </div>
-          </div>
-          <div className={styles.contents}>
-            <div className={styles.inner}>
-              <section className={`${styles.save} ${styles.section}`}>
-                <h2>
-                  <span>
-                    <span className={styles.gradient}>원하는 링크</span>를
-                  </span>
-                  <span>저장하세요</span>
-                </h2>
-                <p>
-                  나중에 읽고 싶은 글, 다시 보고 싶은 영상, 사고 싶은 옷,
-                  기억하고 싶은 모든 것을 한 공간에 저장하세요.
-                </p>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/assets/save-link.png"
-                    alt="Save Link"
-                    width={1100}
-                    height={900}
-                    className={styles.image}
-                  />
-                </div>
-              </section>
+            </section>
 
-              <section
-                className={`${styles.section} ${styles.manage} ${styles.right}`}
-              >
-                <h2>
-                  <span>링크를 폴더로</span>
-                  <span>
-                    <span className={styles.gradient}>관리</span>하세요
-                  </span>
-                </h2>
-                <p>
-                  나만의 폴더를 무제한으로 만들고 다양하게 활용할 수 있습니다.
-                </p>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/assets/manage-link.png"
-                    alt="Manage Link"
-                    width={1100}
-                    height={900}
-                    className={styles.image}
-                  />
-                </div>
-              </section>
-              <section className={`${styles.share} ${styles.section}`}>
-                <h2>
-                  <span>저장한 링크를</span>
-                  <span>
-                    <span className={styles.gradient}>공유</span>해 보세요
-                  </span>
-                </h2>
-                <p>
-                  여러 링크를 폴더에 담고 공유할 수 있습니다. 가족, 친구,
-                  동료들에게 쉽고 빠르게 링크를 공유해 보세요.
-                </p>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/assets/share-link.png"
-                    alt="Share Link"
-                    width={1100}
-                    height={900}
-                    className={styles.image}
-                  />
-                </div>
-              </section>
+            <section
+              className={`${styles.section} ${styles.manage} ${styles.right}`}
+            >
+              <h2>
+                <span>링크를 폴더로</span>
+                <span>
+                  <span className={styles.gradient}>관리</span>하세요
+                </span>
+              </h2>
+              <p>
+                나만의 폴더를 무제한으로 만들고 다양하게 활용할 수 있습니다.
+              </p>
+              <div className={styles.imageContainer}>
+                <Image
+                  src="/assets/manage-link.png"
+                  alt="Manage Link"
+                  width={1100}
+                  height={900}
+                  className={styles.image}
+                />
+              </div>
+            </section>
+            <section className={`${styles.share} ${styles.section}`}>
+              <h2>
+                <span>저장한 링크를</span>
+                <span>
+                  <span className={styles.gradient}>공유</span>해 보세요
+                </span>
+              </h2>
+              <p>
+                여러 링크를 폴더에 담고 공유할 수 있습니다. 가족, 친구,
+                동료들에게 쉽고 빠르게 링크를 공유해 보세요.
+              </p>
+              <div className={styles.imageContainer}>
+                <Image
+                  src="/assets/share-link.png"
+                  alt="Share Link"
+                  width={1100}
+                  height={900}
+                  className={styles.image}
+                />
+              </div>
+            </section>
 
-              <section
-                className={`${styles.section} ${styles.search} ${styles.right}`}
-              >
-                <h2>
-                  <span>저장한 링크를</span>
-                  <span>
-                    <span className={styles.gradient}>검색</span>해 보세요
-                  </span>
-                </h2>
-                <p>중요한 정보들을 검색으로 쉽게 찾아보세요.</p>
-                <div className={styles.imageContainer}>
-                  <Image
-                    src="/assets/search-link.png"
-                    alt="Search Link"
-                    width={1100}
-                    height={900}
-                    className={styles.image}
-                  />
-                </div>
-              </section>
-            </div>
+            <section
+              className={`${styles.section} ${styles.search} ${styles.right}`}
+            >
+              <h2>
+                <span>저장한 링크를</span>
+                <span>
+                  <span className={styles.gradient}>검색</span>해 보세요
+                </span>
+              </h2>
+              <p>중요한 정보들을 검색으로 쉽게 찾아보세요.</p>
+              <div className={styles.imageContainer}>
+                <Image
+                  src="/assets/search-link.png"
+                  alt="Search Link"
+                  width={1100}
+                  height={900}
+                  className={styles.image}
+                />
+              </div>
+            </section>
           </div>
-        </main>
-        <Footer />
-      </QueryHydrate>
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
