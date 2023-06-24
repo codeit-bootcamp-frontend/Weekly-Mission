5e95e2e156658474a578ee053a3139d7b61dc002
wooleejaan <wooleejaan@gmail.com>
2023-06-24 23:18:41 +0900

    feat: AddLinkField에 foter intersecting 기능 추가

--- components/AddLinkField/AddLinkField.tsx
+++ components/AddLinkField/AddLinkField.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { ForwardedRef, forwardRef } from "react";
+import {
+  ForwardedRef,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import { IFolder, ILink } from "@/types/linkbrary";
 
@@ -18,9 +25,43 @@ const AddLinkField = forwardRef(function AddLinkField(
   { userId, folders, links, inView }: IAddLinkFieldProps,
   ref: ForwardedRef<HTMLDivElement>
 ) {
+  const FOOTER_HEIGHT = 160;
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [throttle, setThrottle] = useState(false);
+  const [transition, setTransition] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    if (throttle) return;
+    if (!throttle) {
+      setThrottle(true);
+      setTimeout(() => {
+        if (scrollRef.current) {
+          const currentScrollPosition =
+            scrollRef.current.getBoundingClientRect().bottom + window.scrollY; // pageYOffset == scrollY
+          const IntersectedFooter = document.body.scrollHeight - FOOTER_HEIGHT;
+          setTransition(currentScrollPosition >= IntersectedFooter);
+          setThrottle(false);
+        }
+      }, 300);
+    }
+  }, [throttle]);
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
     <div className={styles.observedWrapper}>
-      <div className={`${styles.addLinkContainer} ${styles[`${inView}`]}`}>
+      <div
+        ref={scrollRef}
+        className={`${styles.addLinkContainer} ${
+          styles[`${inView ? "view" : "not-view"}`]
+        } ${styles[`${transition ? "tr" : "not-tr"}`]}`}
+      >
         <AddLinkBar userId={userId} folders={folders} links={links} />
       </div>
       <div className={styles.observed} ref={ref}></div>
