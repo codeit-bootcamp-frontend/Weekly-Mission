873ba859e601c5f96829f3e5cdab1e64b6bae889
wooleejaan <wooleejaan@gmail.com>
2023-06-25 18:17:26 +0900

    fix: / 페이지에서도 getCurrentUser 훅 제거

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,39 @@ import Footer from "@/components/Footer/Footer";
 import Gnb from "@/components/Gnb/Gnb";
 import ContentsSection from "@/components/MainSection/ContentsSection";
 import HeroSection from "@/components/MainSection/HeroSection";
-import getCurrentUser from "@/utils/getCurrentUser";
+import { getUser } from "@/lib/axios/userRequest";
+import { tempUserDatas } from "@/utils/constants";
+import prisma from "@/utils/prismadb";
+import { getServerSession } from "next-auth";
 
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import styles from "./page.module.scss";
 
 export const revalidate = 3600;
 
 export default async function Home() {
-  const userProfile = await getCurrentUser();
+  const session = await getServerSession(authOptions);
 
+  if (!session?.user?.email) {
+    return null;
+  }
+
+  const currentUser = await prisma?.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  if (!currentUser) {
+    return null;
+  }
+
+  const userId = tempUserDatas[currentUser.id];
+  const userProfile = await getUser(userId);
+
+  if (!userProfile) {
+    throw new Error(`Failed to fetch user data`);
+  }
   return (
     <>
       <Gnb user={userProfile} />
