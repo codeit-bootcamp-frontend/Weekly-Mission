405907b16008f8cca0f0dc7a5ac2807e61b27b12
wooleejaan <wooleejaan@gmail.com>
2023-06-25 21:18:57 +0900

    chore: 에러테스트3

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@ import Footer from "@/components/Footer/Footer";
 import Gnb from "@/components/Gnb/Gnb";
 import ContentsSection from "@/components/MainSection/ContentsSection";
 import HeroSection from "@/components/MainSection/HeroSection";
-import { getUser } from "@/lib/axios/userRequest";
-
+// import { getUser } from "@/lib/axios/userRequest";
 // import { tempUserDatas } from "@/utils/constants";
 // import prisma from "@/utils/prismadb";
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "./api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import styles from "./page.module.scss";
 
 export const revalidate = 3600;
 
 export default async function Home() {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
+
+  console.log(session);
 
   // let userProfile;
   // let userId;
@@ -34,7 +36,7 @@ export default async function Home() {
   //   }
   // }
 
-  const userProfile = await getUser(11);
+  const userProfile = null;
 
   return (
     <>
