779b46d0e7b4b891accee1d47c6d6200a0dacce3
wooleejaan <wooleejaan@gmail.com>
2023-06-25 23:00:03 +0900

    feat: getServerSession 제거

--- app/folder/[slug]/page.tsx
+++ app/folder/[slug]/page.tsx
@@ -1,15 +1,15 @@
 import { notFound } from "next/navigation";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import FolderContents from "@/components/FolderContents/FolderContents";
 import Gnb from "@/components/Gnb/Gnb";
 import { getFolders } from "@/lib/axios/folderRequest";
 import { getLinks } from "@/lib/axios/linkRequest";
 import { getUser } from "@/lib/axios/userRequest";
-import { tempUserDatas } from "@/utils/constants";
-import prisma from "@/utils/prismadb";
-import { getServerSession } from "next-auth";
 
+// import { tempUserDatas } from "@/utils/constants";
+// import prisma from "@/utils/prismadb";
+// import { getServerSession } from "next-auth";
 import styles from "./page.module.scss";
 
 export const revalidate = 1000;
@@ -20,32 +20,35 @@ const Tab = async ({
     slug: string;
   };
 }) => {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  let userProfile;
-  let userId;
+  // let userProfile;
+  // let userId;
 
-  if (!session?.user?.email) {
-    userProfile = null;
-  } else {
-    const currentUser = await prisma?.user.findUnique({
-      where: {
-        email: session.user.email,
-      },
-    });
-    if (!currentUser) {
-      userProfile = null;
-    } else {
-      userId = tempUserDatas[currentUser.id];
-      userProfile = await getUser(userId);
-    }
-  }
+  // if (!session?.user?.email) {
+  //   userProfile = null;
+  // } else {
+  //   const currentUser = await prisma?.user.findUnique({
+  //     where: {
+  //       email: session.user.email,
+  //     },
+  //   });
+  //   if (!currentUser) {
+  //     userProfile = null;
+  //   } else {
+  //     userId = tempUserDatas[currentUser.id];
+  //     userProfile = await getUser(userId);
+  //   }
+  // }
 
-  if (!userProfile) {
-    throw new Error(`Failed to fetch user data`);
-  }
+  // if (!userProfile) {
+  //   throw new Error(`Failed to fetch user data`);
+  // }
 
-  userId = userProfile.id;
+  // userId = userProfile.id;
+
+  const userId = 11;
+  const userProfile = await getUser(userId);
 
   const folderId = Number(params.slug);
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,36 +3,39 @@ import Gnb from "@/components/Gnb/Gnb";
 import ContentsSection from "@/components/MainSection/ContentsSection";
 import HeroSection from "@/components/MainSection/HeroSection";
 import { getUser } from "@/lib/axios/userRequest";
-import { tempUserDatas } from "@/utils/constants";
-import prisma from "@/utils/prismadb";
-import { getServerSession } from "next-auth";
 
-import { authOptions } from "./api/auth/[...nextauth]/route";
+// import { tempUserDatas } from "@/utils/constants";
+// import prisma from "@/utils/prismadb";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
 import styles from "./page.module.scss";
 
 export const revalidate = 3600;
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  let userProfile;
-  let userId;
-  if (!session?.user?.email) {
-    userProfile = null;
-  } else {
-    const currentUser = await prisma?.user.findUnique({
-      where: {
-        email: session.user.email,
-      },
-    });
+  // let userProfile;
+  // let userId;
+  // if (!session?.user?.email) {
+  //   userProfile = null;
+  // } else {
+  //   const currentUser = await prisma?.user.findUnique({
+  //     where: {
+  //       email: session.user.email,
+  //     },
+  //   });
 
-    if (!currentUser) {
-      userProfile = null;
-    } else {
-      userId = tempUserDatas[currentUser.id];
-      userProfile = await getUser(userId);
-    }
-  }
+  //   if (!currentUser) {
+  //     userProfile = null;
+  //   } else {
+  //     userId = tempUserDatas[currentUser.id];
+  //     userProfile = await getUser(userId);
+  //   }
+  // }
+
+  const userId = 11;
+  const userProfile = await getUser(userId);
 
   return (
     <>
