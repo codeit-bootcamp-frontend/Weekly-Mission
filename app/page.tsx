74ae6a70b5997e015e4bb4a0f2705bf19eef9382
wooleejaan <wooleejaan@gmail.com>
2023-06-25 21:13:26 +0900

    chore: 에러 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -2,37 +2,39 @@ import Footer from "@/components/Footer/Footer";
 import Gnb from "@/components/Gnb/Gnb";
 import ContentsSection from "@/components/MainSection/ContentsSection";
 import HeroSection from "@/components/MainSection/HeroSection";
-import { getUser } from "@/lib/axios/userRequest";
-import { tempUserDatas } from "@/utils/constants";
-import prisma from "@/utils/prismadb";
-import { getServerSession } from "next-auth";
 
-import { authOptions } from "./api/auth/[...nextauth]/route";
+// import { getUser } from "@/lib/axios/userRequest";
+// import { tempUserDatas } from "@/utils/constants";
+// import prisma from "@/utils/prismadb";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
 import styles from "./page.module.scss";
 
 export const revalidate = 3600;
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  let userProfile;
-  let userId;
-  if (!session?.user?.email) {
-    userProfile = null;
-  } else {
-    const currentUser = await prisma?.user.findUnique({
-      where: {
-        email: session.user.email,
-      },
-    });
+  // let userProfile;
+  // let userId;
+  // if (!session?.user?.email) {
+  //   userProfile = null;
+  // } else {
+  //   const currentUser = await prisma?.user.findUnique({
+  //     where: {
+  //       email: session.user.email,
+  //     },
+  //   });
 
-    if (!currentUser) {
-      userProfile = null;
-    } else {
-      userId = tempUserDatas[currentUser.id];
-      userProfile = await getUser(userId);
-    }
-  }
+  //   if (!currentUser) {
+  //     userProfile = null;
+  //   } else {
+  //     userId = tempUserDatas[currentUser.id];
+  //     userProfile = await getUser(userId);
+  //   }
+  // }
+
+  const userProfile = null;
 
   return (
     <>
