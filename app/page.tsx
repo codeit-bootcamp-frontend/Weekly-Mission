f2f88d6ffaec2c4defde984d32f3bced222cbbb7
wooleejaan <wooleejaan@gmail.com>
2023-06-25 18:26:43 +0900

    fix: "/" 페이지 getUser 로직 수정

--- app/page.tsx
+++ app/page.tsx
@@ -15,26 +15,25 @@ export const revalidate = 3600;
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
+  let userProfile;
+  let userId;
   if (!session?.user?.email) {
-    return null;
+    userProfile = null;
+  } else {
+    const currentUser = await prisma?.user.findUnique({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!currentUser) {
+      userProfile = null;
+    } else {
+      userId = tempUserDatas[currentUser.id];
+      userProfile = await getUser(userId);
+    }
   }
 
-  const currentUser = await prisma?.user.findUnique({
-    where: {
-      email: session.user.email,
-    },
-  });
-
-  if (!currentUser) {
-    return null;
-  }
-
-  const userId = tempUserDatas[currentUser.id];
-  const userProfile = await getUser(userId);
-
-  if (!userProfile) {
-    throw new Error(`Failed to fetch user data`);
-  }
   return (
     <>
       <Gnb user={userProfile} />
