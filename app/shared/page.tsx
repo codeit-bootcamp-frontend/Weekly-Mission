0b2ffcfe0baafbf93a2b837c50a485467724a01f
hayoung-99 <nhy990130@gmail.com>
2023-06-26 14:26:33 +0900

    feat: shared GNB 로그인 유저 데이터 연결

--- app/shared/page.tsx
+++ app/shared/page.tsx
@@ -1,42 +1,46 @@
 import Card from "@/components/Card";
-import getUserData from "@/api/getUserData";
 import GNB from "@/components/GNB";
 import SearchBar from "@/components/SearchBar";
 import Footer from "@/components/Footer";
 import classNames from "classnames/bind";
 import styles from "./page.module.css";
 import Image from "next/image";
-import getLinks from "@/api/getLinks";
+import getUserId from "@/data/getUserId";
+import getUserData from "@/api/getUserData";
 import getFolders from "@/api/getFolders";
+import getLinks from "@/api/getLinks";
 import { notFound } from "next/navigation";
 
 export default async function Shared({ searchParams }) {
-  const { user: userId, folder: folderId } = searchParams;
+  const { user: sharedUserId, folder: sharedFolderId } = searchParams;
 
-  if (!(userId && folderId) || Number.isNaN(Number(userId)) || Number.isNaN(Number(folderId))) {
+  if (!(sharedUserId && sharedFolderId) || Number.isNaN(Number(sharedUserId)) || Number.isNaN(Number(sharedFolderId))) {
     notFound();
   }
 
-  const folder = await getFolders(userId, folderId);
+  const userId = getUserId();
+  const folder = await getFolders(sharedUserId, sharedFolderId);
+  const sharedUserData = await getUserData(sharedUserId);
   const userData = await getUserData(userId);
-  const links = await getLinks(userId, folderId);
+  const links = await getLinks(sharedUserId, sharedFolderId);
 
   if (folder.length === 0) {
     notFound();
   }
 
   const { name: folderName } = folder[0];
-  const { name: userName, email, image_source } = userData[0];
+  const { name: sharedUserName, image_source: sharedUserProfileImageSource } = sharedUserData[0];
+  const { email, image_source: userProfileImageSorce } = userData[0];
 
   const cx = classNames.bind(styles);
 
   return (
     <>
-      <GNB userEmail={email} userProfileImageSorce={image_source} />
+      <GNB userEmail={email} userProfileImageSorce={userProfileImageSorce} />
       <header className={cx("header")}>
-        <div className={cx("user-content")}>
-          <Image className={cx("user-avatar")} src={image_source} alt="avatar" width={60} height={60} />
-          <p className={cx("user-nickname")}>@{userName}</p>
+        <div className={cx("shared-user-content")}>
+          <Image className={cx("shared-user-avatar")} src={sharedUserProfileImageSource} alt="avatar" width={60} height={60} />
+          <p className={cx("shared-user-nickname")}>@{sharedUserName}</p>
         </div>
         <h1 className={cx("header-title")}>{folderName}</h1>
       </header>
