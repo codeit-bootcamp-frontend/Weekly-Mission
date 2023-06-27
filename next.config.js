3e3578d3aa4e076cb16e05774b9d5bd09ba3a55a
hayoung-99 <nhy990130@gmail.com>
2023-06-27 13:33:21 +0900

    chore: image path 전체 허용

--- next.config.js
+++ next.config.js
@@ -2,18 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "codeit-frontend.codeit.com",
-      "reactjs.org",
-      "assets.vercel.com",
-      "github.com",
-      "storybook.js.org",
-      "testing-library.com",
-      "static.cdninstagram.com",
-      "s.pstatic.net",
-      "codeit-images.codeit.com", // folder user image
-      "codeit-front.s3.ap-northeast-2.amazonaws.com", // login user image
-      "ca.slack-edge.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
     ],
   },
 };
