4aded6cc7b747c402366baf4f506324c0f1a45cb
wooleejaan <wooleejaan@gmail.com>
2023-07-01 22:11:54 +0900

    fix: axios instance 주석 해제

--- lib/axios/instance.ts
+++ lib/axios/instance.ts
@@ -6,11 +6,11 @@ import axios, {
   InternalAxiosRequestConfig,
 } from "axios";
 
-// const logOnDev = (message: string) => {
-//   if (process.env.NODE_ENV === "development") {
-//     console.log(message);
-//   }
-// };
+const logOnDev = (message: string) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(message);
+  }
+};
 
 export const instance: CustomAxiosInterface = axios.create({
   baseURL:
@@ -30,10 +30,10 @@ instance.interceptors.request.use(
     /**
      * request 직전 공통으로 진행할 작업
      */
-    // if (process.env.NODE_ENV === "development") {
-    //   const { method, url } = config;
-    //   logOnDev(`🚀 [API] ${method?.toUpperCase()} ${url} | Request`);
-    // }
+    if (process.env.NODE_ENV === "development") {
+      const { method, url } = config;
+      logOnDev(`🚀 [API] ${method?.toUpperCase()} ${url} | Request`);
+    }
 
     return config;
   },
@@ -51,11 +51,11 @@ instance.interceptors.response.use(
     /**
      * http status가 20X이고, http response가 then으로 넘어가기 직전 호출
      */
-    // if (process.env.NODE_ENV === "development") {
-    //   const { method, url } = response.config;
-    //   const { status } = response;
-    //   logOnDev(`🚁 [API] ${method?.toUpperCase()} ${url} | Response ${status}`);
-    // }
+    if (process.env.NODE_ENV === "development") {
+      const { method, url } = response.config;
+      const { status } = response;
+      logOnDev(`🚁 [API] ${method?.toUpperCase()} ${url} | Response ${status}`);
+    }
 
     return response;
   },
@@ -63,42 +63,42 @@ instance.interceptors.response.use(
     /**
      * http status가 20X가 아니고, http response가 catch로 넘어가기 직전 호출
      */
-    // if (process.env.NODE_ENV === "development") {
-    //   if (axios.isAxiosError(error)) {
-    //     const { message } = error;
-    //     const { method, url } = error.config as InternalAxiosRequestConfig;
-    //     const { status, statusText } = error.response as AxiosResponse;
+    if (process.env.NODE_ENV === "development") {
+      if (axios.isAxiosError(error)) {
+        const { message } = error;
+        const { method, url } = error.config as InternalAxiosRequestConfig;
+        const { status, statusText } = error.response as AxiosResponse;
 
-    //     logOnDev(
-    //       `🚨 [API] ${method?.toUpperCase()} ${url} | Error ${status} ${statusText} | ${message}`
-    //     );
+        logOnDev(
+          `🚨 [API] ${method?.toUpperCase()} ${url} | Error ${status} ${statusText} | ${message}`
+        );
 
-    //     switch (status) {
-    //       case 401: {
-    //         // 로그인 필요 메시지 연결
-    //         break;
-    //       }
-    //       case 403: {
-    //         // 권한 필요 메시지 연결
-    //         break;
-    //       }
-    //       case 404: {
-    //         // 잘못된 요청 메시지 연결
-    //         break;
-    //       }
-    //       case 500: {
-    //         // 서버 문제 발생 메시지 연결
-    //         break;
-    //       }
-    //       default: {
-    //         // 알 수 없는 오류 발생 메시지 연결
-    //         break;
-    //       }
-    //     }
-    //   } else {
-    //     logOnDev(`🚨 [API] | Error ${error.message}`);
-    //   }
-    // }
+        switch (status) {
+          case 401: {
+            // 로그인 필요 메시지 연결
+            break;
+          }
+          case 403: {
+            // 권한 필요 메시지 연결
+            break;
+          }
+          case 404: {
+            // 잘못된 요청 메시지 연결
+            break;
+          }
+          case 500: {
+            // 서버 문제 발생 메시지 연결
+            break;
+          }
+          default: {
+            // 알 수 없는 오류 발생 메시지 연결
+            break;
+          }
+        }
+      } else {
+        logOnDev(`🚨 [API] | Error ${error.message}`);
+      }
+    }
     return Promise.reject(error);
   }
 );
