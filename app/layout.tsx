b1453be25cc728bc1145390d23e0f974ed7bdf47
wooleejaan <wooleejaan@gmail.com>
2023-07-01 22:10:55 +0900

    fix: sitemap, robots url 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,14 @@ import "@/styles/globals.scss";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    "https://weekly-mission-git-fork-wooleejaan-henry-5968a4-codeit-bootcamp.vercel.app"
+    "https://weekly-mission-git-henry-react-codeit-bootcamp.vercel.app"
   ),
   title: "Codeit Weekly Mission",
   description: "세상의 모든 정보를 쉽게 저장하고 관리해 보세요.",
   openGraph: {
     title: "Linkbrary",
     description: "세상의 모든 정보를 쉽게 저장하고 관리해 보세요.",
-    url: "https://weekly-mission-git-fork-wooleejaan-henry-5968a4-codeit-bootcamp.vercel.app",
+    url: "https://weekly-mission-git-henry-react-codeit-bootcamp.vercel.app",
     siteName: "linkbrary",
     images: {
       url: "https://i.ibb.co/DpfKPC9/img-info.png",
--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: "/private/",
     },
     sitemap:
-      "https://weekly-mission-git-fork-wooleejaan-henry-5968a4-codeit-bootcamp.vercel.app/sitemap.xml",
+      "https://weekly-mission-git-henry-react-codeit-bootcamp.vercel.app/sitemap.xml",
   };
 }
