25faf40a5b9d953ac413081245956328fd8fc7fd
wooleejaan <wooleejaan@gmail.com>
2023-07-01 20:30:22 +0900

    chore: root error.tsx 작성

--- app/error.tsx
+++ app/error.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-export default function Error({ reset }: { error: Error; reset: () => void }) {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
   return (
     <div>
       <h2>
         Something went wrong! 메인페이지 혹은 각 페이지의 레이아웃에서 발생하는
         에러 처리
       </h2>
+      <p>{error.message}</p>
       <button onClick={() => reset()}>Try again</button>
     </div>
   );
