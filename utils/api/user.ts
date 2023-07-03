d73f10cc61ac0d5aa009a80283720aa2ed7b078d
drizzle96 <dlxkffldk37@naver.com>
2023-07-03 13:01:02 +0900

    chore: getUSer return 형태 수정

--- utils/api/user.ts
+++ utils/api/user.ts
@@ -2,8 +2,7 @@ import instance from "./instance";
 import { User } from "./types";
 
 const getUser = async (id: string): Promise<User> => {
-  const res = await instance.get<never, User>(`users/${id}`);
-  return res;
+  return await instance.get<never, User>(`users/${id}`);
 };
 
 export { getUser };
