99851d99d4595c6de2554654b58f176b66d6c5de
09sally <9609sally@gmail.com>
2023-07-19 04:07:52 +0900

    feat: global css 파일 생성

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
