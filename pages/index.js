2cc3d9e074041b95dfed029f799a599b1a2ab817
09sally <9609sally@gmail.com>
2023-07-19 16:34:49 +0900

    feat: 홈페이지 소개 칸

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Gnb from "@/components/Gnb";
+import IntroduceBoard from "@/components/IntroduceBoard";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <Gnb />
+      <IntroduceBoard />
       <Footer />
     </>
   );
