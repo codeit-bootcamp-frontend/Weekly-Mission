4d36e66c30dc349e4405b426502cd689c4ff4d40
09sally <9609sally@gmail.com>
2023-07-19 09:01:10 +0900

    fix: rewrite로 CORS 에러 해결

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://bootcamp-api.codeit.kr/api/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
