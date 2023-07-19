f72306c0db5f276521bfc737eb1a53d3d44d34e4
09sally <9609sally@gmail.com>
2023-07-19 16:35:50 +0900

    feat: shared 페이지 api 연동

--- next.config.js
+++ next.config.js
@@ -9,6 +9,22 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "codeit-front.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "codeit-images.codeit.com",
+        port: "",
+        pathname: "/badges/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
---
+++ pages/shared.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Image from "next/image";
+import Gnb from "@/components/Gnb";
+import SearchBar from "@/components/SearchBar";
+import styles from "@/styles/Shared.module.css";
+
+export default function Shared() {
+  const [sampleUser, setSampleUser] = useState();
+  const [sampleFolder, setSampleFolder] = useState();
+
+  async function getSampleUser() {
+    const res = await axios.get(`/sample/user`);
+    const nextSampleUser = res.data;
+    setSampleUser(nextSampleUser);
+  }
+
+  async function getSampleFolder() {
+    const res = await axios.get(`/sample/folder`);
+    const nextSampleFolder = res.data;
+    setSampleFolder(nextSampleFolder);
+  }
+
+  useEffect(() => {
+    getSampleUser();
+    getSampleFolder();
+  }, []);
+
+  const { folder } = sampleFolder;
+  const { name, owner, links } = folder;
+
+  return (
+    <>
+      <Gnb user={sampleUser} />
+      <div className={styles.ownerBoard}>
+        <div className={styles.ownerImage}>
+          <Image
+            className={styles.image}
+            fill
+            src={owner.profileImageSource}
+            alt={owner.name}
+          />
+        </div>
+        <div className={styles.ownerName}>@{owner.name}</div>
+        <div className={styles.folderName}>{name}</div>
+      </div>
+      <div className={styles.linksContainer}>
+        <SearchBar />
+      </div>
+    </>
+  );
+}
