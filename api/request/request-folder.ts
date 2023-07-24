972d69cc72b820515f8bbf444003ade43555fc78
hayoung-99 <nhy990130@gmail.com>
2023-07-25 01:36:44 +0900

    feat: folder 관련 API 함수 제작

--- api/request/request-folder.ts
+++ api/request/request-folder.ts
@@ -1,15 +1,39 @@
 import instance from "@/api/instance";
 
-const getAllFolders = async (userId: number) => {
+const getUserAllFolders = async (userId: number) => {
   const response = await instance.get(`users/${userId}/folders`);
 
   return response.data.data;
 };
 
-const getFolder = async (userId: number, folderId: number) => {
+const getUserFolder = async (userId: number, folderId: number) => {
   const response = await instance.get(`/users/${userId}/folders/${folderId}`);
 
   return response.data.data;
 };
 
-export { getAllFolders, getFolder };
+const getFolder = async (folderId: number) => {
+  const response = await instance.get(`/folders/${folderId}`);
+
+  return response.data.data;
+};
+
+const changeFolderName = async (folderId: number, name: string) => {
+  const response = await instance.put(`/folders/${folderId}`, { name });
+
+  return response.data;
+};
+
+const createFolder = async (name: string) => {
+  const response = await instance.post("/folders", { name });
+
+  return response.data;
+};
+
+const deleteFolder = async (folderId: number) => {
+  const response = await instance.delete(`/folders/${folderId}`);
+
+  return response.data;
+};
+
+export { getUserAllFolders, getUserFolder, getFolder, changeFolderName, createFolder, deleteFolder };
