821e616d4704db64b39df017ddf258c58197e35b
hayoung-99 <nhy990130@gmail.com>
2023-07-25 01:50:12 +0900

    chore: CORS 에러 관련 rewrites 설정

--- next.config.js
+++ next.config.js
@@ -9,22 +9,11 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    BASE_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : `${process.env.NEXT_PUBLIC_VERCEL_URL}`,
-  },
   async rewrites() {
     return [
       {
-        source: "/users/:path*",
-        destination: "https://bootcamp-api.codeit.kr/api/users/:path*",
-      },
-      {
-        source: "/folders/:path*",
-        destination: "https://bootcamp-api.codeit.kr/api/folders/:path*",
-      },
-      {
-        source: "/links/:path*",
-        destination: "https://bootcamp-api.codeit.kr/api/links/:path*",
+        source: "/:path*",
+        destination: "https://bootcamp-api.codeit.kr/api/:path*",
       },
     ];
   },
