8ba16f5b081923ee10e4630e0b38500c1ece81e7
hayoung-99 <nhy990130@gmail.com>
2023-07-25 01:59:43 +0900

    feat: 이메일 중복 확인 API 함수 제작

--- api/request/request-user.ts
+++ api/request/request-user.ts
@@ -1,9 +1,19 @@
 import instance from "@/api/instance";
 
+const checkExistEmail = async (email: string) => {
+  try {
+    const response = await instance.post("/check-email", { email });
+
+    return response;
+  } catch (error) {
+    alert("중복되는 이메일입니다.");
+  }
+};
+
 const getUser = async (userId: number) => {
   const response = await instance.get(`/users/${userId}`);
 
   return response.data.data;
 };
 
-export { getUser };
+export { checkExistEmail, getUser };
