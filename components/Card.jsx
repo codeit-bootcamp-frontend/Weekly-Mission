9c3ae16a50544ff467984edcdd95221142eca70c
09sally <9609sally@gmail.com>
2023-07-27 21:26:22 +0900

    feat: utils 함수로 시간 계산 구현

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,25 +1,43 @@
 import Link from "next/link";
 import Star from "@/components/Star";
 import styles from "@/components/Card.module.css";
+import classNames from "classnames/bind";
+import { formatDate } from "@/utils/formatDate";
+import { formatTimeSpan } from "@/utils/formatTimeSpan";
 
-export default function Card({ link }) {
+export default function Card({ link, today }) {
   const { createdAt, url, title, description, imageSource } = link;
+  const cx = classNames.bind(styles);
+
+  const createdDate = new Date(createdAt);
+  const timeDiffMinutes = (today - createdDate) / 1000 / 60;
+
+  const formattedDate = formatDate(createdDate);
+  const formattedTimeSpan = formatTimeSpan(timeDiffMinutes);
+
   return (
-    <div className={styles.card}>
-      <Star className={styles.star} />
-      <Link className={styles.link} href={url}>
-        <div classsName={styles.cardImage}>
-          <img
-            className={styles.cardImage}
-            fill
-            src={imageSource}
-            alt={title}
-          />
+    <div className={cx("card")}>
+      <div className={cx("star-container")}>
+        <Star />
+      </div>
+      <Link href={url} target="_blank">
+        <div className={cx("card-image-container")}>
+          {imageSource ? (
+            <img
+              className={cx("card-image")}
+              fill
+              src={imageSource}
+              alt={title}
+            />
+          ) : (
+            <img className={cx("card-image")} fill src="/images/no-image.svg" />
+          )}
         </div>
-        <div className={styles.cardInfo}>
-          <div>10 minutes ago</div>
-          <div>{description}</div>
-          <div>{createdAt}</div>
+        <div className={cx("card-info")}>
+          <img className={cx("kebab")} src="/images/kebab.svg" alt="kebab" />
+          <div className={cx("time-span")}>{formattedTimeSpan}</div>
+          <div className={cx("description")}>{description}</div>
+          <div className={cx("created-date")}>{formattedDate}</div>
         </div>
       </Link>
     </div>
