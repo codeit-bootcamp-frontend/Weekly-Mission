f60891b9a7de0e8a1b106c3658a2d233f2cd26c3
09sally <9609sally@gmail.com>
2023-07-27 19:48:05 +0900

    style: classnames bind 적용

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,14 +1,17 @@
 import styles from "@/components/SearchBar.module.css";
+import classNames from "classnames/bind";
 
 export default function SearchBar() {
+  const cx = classNames.bind(styles);
+
   return (
-    <div className={styles.searchBar}>
+    <div className={cx("search-bar")}>
       <input
-        className={styles.searchInput}
+        className={cx("search-input")}
         type="text"
         placeholder="원하는 링크를 검색해 보세요"
       />
-      <img className={styles.searchIcon} src="/images/search-icon.png" />
+      <img className={cx("search-icon")} src="/images/search-icon.png" />
     </div>
   );
 }
