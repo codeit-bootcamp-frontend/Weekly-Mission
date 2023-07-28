c4a464d69979caeca9346624bffee5f2d9528db6
09sally <9609sally@gmail.com>
2023-07-28 23:14:03 +0900

    feat: 모달용 버튼 추가, 반응형 스타일용 class 이름 생성

--- components/Button.jsx
+++ components/Button.jsx
@@ -8,7 +8,7 @@ export function SigninButton() {
   return (
     <>
       <Link href="/signin">
-        <div className={cx("button", "signin")}>로그인</div>
+        <div className={cx("button", "button-mobile", "signin")}>로그인</div>
       </Link>
     </>
   );
@@ -18,7 +18,9 @@ export function AddLinkButton() {
   return (
     <div className={cx("addlink-wrapper")}>
       <Link href="">
-        <div className={cx("button", "add-link")}>링크 추가하기</div>
+        <div className={cx("button", "button-mobile", "add-link")}>
+          링크 추가하기
+        </div>
       </Link>
     </div>
   );
@@ -30,4 +32,12 @@ export function AddButton() {
       <div className={cx("button", "add")}>추가하기</div>
     </>
   );
+}
+
+export function ModalButton({ children }) {
+  return <div className={cx("button", "modal-button")}>{children}</div>;
+}
+
+export function ModalDeleteButton() {
+  return <div className={cx("delete-button")}>삭제하기</div>;
 }
