f5e232e574d87811bf794a217e9e3bff23d161dd
09sally <9609sally@gmail.com>
2023-07-28 15:14:47 +0900

    feat: margin, padding 수정

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,48 +6,51 @@ export default function Footer() {
   const cx = classNames.bind(styles);
 
   return (
-    <div className={cx("footer-container")}>
-      <div className={cx("footer")}>
-        <div className={cx("copyright")}>©codeit-2023</div>
-        <div className={cx("bottom-links")}>
-          <Link href="/privacy">
-            <div className={cx("bottom-link")}>Privacy Policy</div>
-          </Link>
-          <Link href="/faq">
-            <div className={cx("bottom-link")}>FAQ</div>
-          </Link>
-        </div>
-        <div className={cx("sns-icons")}>
-          <Link href="https://www.facebook.com">
-            <img
-              className={cx("sns-icon")}
-              src="/images/sns-icons/facebook.png"
-              alt="페이스북"
-            />
-          </Link>
-          <Link href="https://twitter.com">
-            <img
-              className={cx("sns-icon")}
-              src="/images/sns-icons/twitter.png"
-              alt="트위터"
-            />
-          </Link>
-          <Link href="https://www.youtube.com">
-            <img
-              className={cx("sns-icon")}
-              src="/images/sns-icons/youtube.png"
-              alt="유튜브"
-            />
-          </Link>
-          <Link href="https://www.instagram.com">
-            <img
-              className={cx("sns-icon")}
-              src="/images/sns-icons/instagram.png"
-              alt="인스타그램"
-            />
-          </Link>
+    <>
+      <div className={cx("white-space")}></div>
+      <div className={cx("footer-container")}>
+        <div className={cx("footer")}>
+          <div className={cx("copyright")}>©codeit-2023</div>
+          <div className={cx("bottom-links")}>
+            <Link href="/privacy">
+              <div className={cx("bottom-link")}>Privacy Policy</div>
+            </Link>
+            <Link href="/faq">
+              <div className={cx("bottom-link")}>FAQ</div>
+            </Link>
+          </div>
+          <div className={cx("sns-icons")}>
+            <Link href="https://www.facebook.com">
+              <img
+                className={cx("sns-icon")}
+                src="/images/sns-icons/facebook.png"
+                alt="페이스북"
+              />
+            </Link>
+            <Link href="https://twitter.com">
+              <img
+                className={cx("sns-icon")}
+                src="/images/sns-icons/twitter.png"
+                alt="트위터"
+              />
+            </Link>
+            <Link href="https://www.youtube.com">
+              <img
+                className={cx("sns-icon")}
+                src="/images/sns-icons/youtube.png"
+                alt="유튜브"
+              />
+            </Link>
+            <Link href="https://www.instagram.com">
+              <img
+                className={cx("sns-icon")}
+                src="/images/sns-icons/instagram.png"
+                alt="인스타그램"
+              />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
